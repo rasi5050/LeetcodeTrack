9d84a4b3e8db7d08834ee9ac43ffc781735e10d1
Rasi <rasi5050@gmail.com>
2022-11-11 14:29:52 -0500

    Time: 95 ms (96.72%), Space: 14 MB (35.35%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -18,4 +18,4 @@ class Solution:
     #status: correct:
     #ref:11/10/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day35/36:do,1.1.SIG-60min(lastDate:6:46am PST on Friday, January 6th)-x2pomo(5:30-6:30),2.addQuestionsToGoogleDrive-x2pomo(6:30-7:30),3.spiralMatrixTimed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.rotateImageTimed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.validSudokuTimed25Mins-x1pomo(10:30-11:00),3.implement-x2pomo(11:00-12:00)=x13pomo(5:30-12:00)
     #analysis: Time O(9*9)=O(1)
-    #          Space O(1)
+    #          Space O(9*9)=O(1)
