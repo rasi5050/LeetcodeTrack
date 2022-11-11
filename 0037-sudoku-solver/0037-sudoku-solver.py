7665e842890af49023f307998d7527a9758e1702
Rasi <rasi5050@gmail.com>
2022-11-11 14:27:52 -0500

    Time: 334 ms (70.38%), Space: 14.1 MB (43.66%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -0,0 +1,53 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        #intuition: complete help from https://www.youtube.com/watch?v=lLixGoGuClc
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!='.':
+                    num = int(board[i][j])
+                    rows[i].add(num)
+                    cols[j].add(num)
+                    #for every row,its effective to 3 boxes
+                    box_id = i//3 * 3 + j//3
+                    boxes[box_id].add(num)
+                    
+        def backTrack(i, j):
+            #base case
+            nonlocal solved
+            if i == 9:
+                solved = True
+                return
+            
+            new_i = i + (j+1)//9
+            new_j = (j+1) % 9
+            
+            if board[i][j] != '.':
+                backTrack(new_i, new_j)
+            else:
+                for num in range(1, 10):
+                    box_id = i//3 * 3 + j//3
+                    if num not in rows[i] and num not in cols[j] and num not in boxes[box_id]:
+                        rows[i].add(num)
+                        cols[j].add(num)
+                        boxes[box_id].add(num)
+                        board[i][j] = str(num)
+                        
+                        backTrack(new_i, new_j)
+                        
+                        if not solved:
+                            rows[i].remove(num)
+                            cols[j].remove(num)
+                            boxes[box_id].remove(num)
+                            board[i][j] = '.'
+        solved = False
+        backTrack(0,0)
+        
+        #status: exact copy as video; no @Rasi effort
+        #ref: 11/1/2022P2:rack1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day29/30:do,1.sudokuSolverLogic+implement-x2pomo(7:30-8:30),2.study:sortingAndSearching-x2pomo(8:30-9:30),3.binarySearchLogic-x1pomo(9:30-10:00)=x5pomo(7:30-10:00)
