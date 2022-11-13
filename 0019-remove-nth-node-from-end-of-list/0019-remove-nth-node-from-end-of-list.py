9ad73a49f1ffc2ea3638fe15ddf8bf747a5f1748
Rasi <rasi5050@gmail.com>
2022-11-13 15:48:37 -0500

    Time: 46 ms (79.80%), Space: 13.9 MB (21.49%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,18 +5,21 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head.next:
+        if not head.next: #null case
             return None
         back=front=head
-        for _ in range(n):
+        for _ in range(n):  #moving front pointer ahead
             front=front.next
-        print(back,front)
-        if front==None:
+        if front==None:     #n==len(list)
             return head.next
         while front.next:
             back=back.next
             front=front.next
         back.next=back.next.next
         return head
+    
+    #status: correct
+    #ref: 11/13/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day37/38:do,1.mergeKSortedListsTimed25Mins-x1pomo(12:00-12:30),2.implement-x2pomo(12:30-13:30),3.removeNthNodeFromEndOfListTimed25Mins-x1pomo(13:30-14:00),4.implement-x2pomo(14:00-15:00),5.reorderListTimed25Mins-x1pomo(15:00-15:30),6.implement-x2pomo(15:30-16:30),7.studyQueue-x1pomo(16:30-17:00),8.stackUsingQueuesTimed25Mins-x1pomo(17:00-17:30),9.implement-x2pomo(17:30-18:30)=x13pomo(12:00-18:30)
+
         
             
