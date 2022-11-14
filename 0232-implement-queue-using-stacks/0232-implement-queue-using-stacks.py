00b01ad945d853af2e9677ebaa7a5601fdb1da23
Rasi <rasi5050@gmail.com>
2022-11-14 09:18:09 -0500

    Time: 34 ms (89.33%), Space: 13.9 MB (75.63%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -2,27 +2,27 @@ from collections import deque
 class MyQueue:
 
     def __init__(self):
-        self.q1=deque()
-        self.qTemp=deque()
+        self.q1=[]
+        self.qTemp=[]
 
     def push(self, x: int) -> None:
         self.q1.append(x)
 
     def pop(self) -> int:
         while self.q1:
-            self.qTemp.append(self.q1.popleft())
-        tempVar=self.qTemp.popleft()
+            self.qTemp.append(self.q1.pop())
+        tempVar=self.qTemp.pop()
         while self.qTemp:
-            self.q1.append(self.qTemp.popleft())
+            self.q1.append(self.qTemp.pop())
         return tempVar
 
     def peek(self) -> int:
         while self.q1:
-            self.qTemp.append(self.q1.popleft())
-        tempVar=self.qTemp.popleft()
+            self.qTemp.append(self.q1.pop())
+        tempVar=self.qTemp.pop()
         self.q1.append(tempVar)
         while self.qTemp:
-            self.q1.append(self.qTemp.popleft())
+            self.q1.append(self.qTemp.pop())
         return tempVar
 
     def empty(self) -> bool:
