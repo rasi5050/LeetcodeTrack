24120ac5a356aada84d487a946196fdddde9f733
Rasi <rasi5050@gmail.com>
2022-11-14 09:35:03 -0500

    Time: 66 ms (12.44%), Space: 14 MB (75.63%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,40 +1,37 @@
-from collections import deque
 class MyQueue:
-
+    
     def __init__(self):
-        self.q1=[]
-        self.qTemp=[]
-
+        self.enqueue_stack = []
+        self.dequeue_stack = []
+    
+    # enqueue
     def push(self, x: int) -> None:
-        self.q1.append(x)
-
+        self.enqueue_stack.append(x)
+        
+    # dequeue
     def pop(self) -> int:
-        while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
-        while self.qTemp:
-            self.q1.append(self.qTemp.pop())
-        return tempVar
+        # if items from dequeue stack are over refill it from enqueue stack
+        if not self.dequeue_stack:
+            self.fill()
+            
+        # otherwise pop items from dequeue stack
+        return self.dequeue_stack.pop()
 
     def peek(self) -> int:
-        while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
-        self.q1.append(tempVar)
-        while self.qTemp:
-            self.q1.append(self.qTemp.pop())
-        return tempVar
+        if not self.dequeue_stack:
+            self.fill()
 
+        # we could use dequeue_stack[-1] operation but this is not valid
+        # according to the details of this problem
+        temp = self.dequeue_stack.pop()
+        self.dequeue_stack.append(temp)
+        return temp
+    
     def empty(self) -> bool:
-        return True if len(self.q1)==0 else False
-    #analysis time O(nk), n: no of elements, k=number of operations, since peek and pop take O(2k)
-    #space: O(2n)=(n)
+        return not len(self.dequeue_stack) and not len(self.enqueue_stack)
     
+    # get items from enqueue to dequeue stack so we could pop items with 0(1) time
     
-    
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+    def fill(self):
+        while self.enqueue_stack:
+            self.dequeue_stack.append(self.enqueue_stack.pop())
