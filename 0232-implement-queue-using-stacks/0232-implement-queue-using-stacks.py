e0ecba938b0908597603f913eb5908db5aa2d3d9
Rasi <rasi5050@gmail.com>
2022-11-14 09:06:32 -0500

    Time: 61 ms (28.23%), Space: 14 MB (75.63%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,33 +1,37 @@
+from collections import deque
 class MyQueue:
 
     def __init__(self):
-        self.q1=[]
-        self.qTemp=[]
+        self.q1=deque()
+        self.qTemp=deque()
 
     def push(self, x: int) -> None:
         self.q1.append(x)
 
     def pop(self) -> int:
         while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
+            self.qTemp.append(self.q1.popleft())
+        tempVar=self.qTemp.popleft()
         while self.qTemp:
-            self.q1.append(self.qTemp.pop())
+            self.q1.append(self.qTemp.popleft())
         return tempVar
 
     def peek(self) -> int:
         while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
+            self.qTemp.append(self.q1.popleft())
+        tempVar=self.qTemp.popleft()
         self.q1.append(tempVar)
         while self.qTemp:
-            self.q1.append(self.qTemp.pop())
+            self.q1.append(self.qTemp.popleft())
         return tempVar
 
     def empty(self) -> bool:
         return True if len(self.q1)==0 else False
-
-
+    #analysis time O(nk), n: no of elements, k=number of operations, since peek and pop take O(2k)
+    #space: O(2n)=(n)
+    
+    
+    
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
