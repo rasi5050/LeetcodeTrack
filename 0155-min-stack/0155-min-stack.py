139738abd1d0c7513f26dcbd5cdc984e2d82bd52
Rasi <rasi5050@gmail.com>
2022-11-15 09:11:29 -0500

    Time: 62 ms (94.18%), Space: 18 MB (58.48%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -17,6 +17,12 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minStack[-1]
+    
+    #status: correct
+    #analysis: Time O(n): depends on the size of the input
+    #Space O(n): O(2n) space is used, n for each stacks
+    #ref: 11/15/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day39/40:do,1.studyStacks-x1pomo(6:00-6:30),2.validParenthesesTimed25Mins-x1pomo(6:30-7:00),3.implement-x2pomo(7:00-8:00),4.minStackTimed25Mins-x1pomo(8:00-8:30),5.implement-x2pomo(8:30-9:30),6.asteroidCollisionTimefd25Mins-x1pomo(9:30-10:00),7.implement-x2pomo(10:00-11:00)=x10pomo(6:00-11:00)
+
 
 
 # Your MinStack object will be instantiated and called as such:
