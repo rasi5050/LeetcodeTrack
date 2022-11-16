aeddad6705d904f6d5b0abd9e6acd103bd882475
Rasi <rasi5050@gmail.com>
2022-11-16 06:54:50 -0500

    Time: 77 ms (85.45%), Space: 14.3 MB (96.21%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -2,7 +2,6 @@ class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack=[]
         for elem in tokens:
-            # print(stack)
             if elem in {'+','-','*','/'}:
                 b,a=stack.pop(),stack.pop()
                 if elem == '+':
@@ -15,5 +14,5 @@ class Solution:
                     stack.append(int(a/b))
             else:
                 stack.append(int(elem))
-        return stack[0]
+        return stack.pop()
      
