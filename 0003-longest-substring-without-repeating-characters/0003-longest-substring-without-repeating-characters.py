1dcabaa35d61f6e000ef485cbf255ad4edb15d0b
Rasi <rasi5050@gmail.com>
2022-11-17 08:22:22 -0500

    Time: 85 ms (83.74%), Space: 13.9 MB (93.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,7 +1,7 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         #intuition: use two pointer
-        
+        """correct code commented for optimization
         counter=0
         subString=deque()
         subStringSet=set()
@@ -20,8 +20,20 @@ class Solution:
             j+=1
             counter=max(counter, j-i)   
         return counter
-    
+        """
     #status: correct
     #analysis: Space O(2n)=O(n)
-    #Time O(n), earlier it was O(n^2), since The search was within the deque which will take O(n), now its replaced with set(), hence check will take only O(1)
+    #Time O(n), earlier it was O(n^2), since The search was within the deque which will take O(n), now its replaced with set(), hence check will take only O(1)==>correction, this is still O(n^2) since inner loop pops element one by one 
     #ref:11/17/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day40/40:(P1:doPaypalLeetcodeQuestion1/5(https://github.com/hxu296/leetcode-company-wise-problems-2022/blob/main/companies/Paypal.csv),do,1.twoSumTimed25Mins-x1pomo(5:30-6:00),2.longestSubstringWithoutRepeatingCharactersTimed25Mins-x1pomo(6:00-6:30),3.implement-x2pomo(6:30-7:30),4.medianOfTwoSortedArraysTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.zigzagConversionTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30)=x10pomo(5:30-10:30)
+
+        counter=0
+        map={}
+        i=j=0    
+        while j<len(s):
+            if s[j] in map:
+                i=max(i,map[s[j]])
+            map[s[j]]=j+1
+            j+=1
+            # print(map, i, j)
+            counter=max(counter, j-i)   
+        return counter
