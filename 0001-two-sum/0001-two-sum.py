e26abfb6a39719a274aedc7ba0a4c478a09ea840
Rasi <rasi5050@gmail.com>
2022-11-17 06:22:42 -0500

    Time: 73 ms (87.12%), Space: 15.1 MB (54.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """workingCode
         # map={}
         # for i,num in enumerate(nums):
         #     map[num]=i
         # for i,num in enumerate(nums):
         #     if target-num in map and map[target-num]!=i:
         #         return [i, map[target-num]]
+        """
     #status: correct:
     #analysis: Space O(n)
     #Time O(n)
