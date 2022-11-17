d98a840a61f954383a2d38dd0870d29d7b1f31f9
Rasi <rasi5050@gmail.com>
2022-11-17 06:21:22 -0500

    Time: 153 ms (36.14%), Space: 15.2 MB (54.60%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # map={}
+        # for i,num in enumerate(nums):
+        #     map[num]=i
+        # for i,num in enumerate(nums):
+        #     if target-num in map and map[target-num]!=i:
+        #         return [i, map[target-num]]
+    #status: correct:
+    #analysis: Space O(n)
+    #Time O(n)
+    
+    #improvement: idea from leetocde discussion solution (https://leetcode.com/problems/two-sum/discuss/2669414/Python-solution-oror-Easy-to-understand)
+        
+        # idea:two iterations of the elements are reduced to one, add elements after checking if target-num in map, then if not add to map. this avoid eg) 3 to 3 mapped case
         map={}
         for i,num in enumerate(nums):
-            map[num]=i
-        for i,num in enumerate(nums):
-            if target-num in map and map[target-num]!=i:
-                return [i, map[target-num]]
+            if target-num in map:return [i, map[target-num]]
+            map[num] = i
+        
