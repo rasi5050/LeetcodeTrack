4324a94f23501c63339b8b01dba89bf10b550913
Rasi <rasi5050@gmail.com>
2022-11-17 07:31:55 -0500

    Time: 76 ms (86.97%), Space: 14 MB (93.62%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -4,16 +4,19 @@ class Solution:
         
         counter=0
         subString=deque()
+        subStringSet=set()
         i=j=0
         
         while j<len(s):
-            if s[j] in subString:
+            if s[j] in subStringSet:
                 while subString[0]!=s[j]:
+                    subStringSet.remove(subString[0])
                     subString.popleft()
                     i+=1
                 subString.popleft()
                 i+=1
             subString.append(s[j])
+            subStringSet.add(s[j])
             j+=1
             counter=max(counter, j-i)   
         return counter
