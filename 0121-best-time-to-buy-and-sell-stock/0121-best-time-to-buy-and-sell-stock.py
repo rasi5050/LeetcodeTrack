c7b485628c3e673832dc2b01e389844f18b3b414
Rasi <rasi5050@gmail.com>
2022-11-19 07:20:56 -0500

    Time: 1213 ms (82.23%), Space: 25 MB (86.44%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -2,22 +2,38 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         #intuition: maintain cur min, max table
         
-        leftMin=[]
-        curMin=prices[0]
-        for price in prices:
-            curMin=min(price, curMin)
-            leftMin.append(curMin)
+#         leftMin=[]
+#         curMin=prices[0]
+#         for price in prices:
+#             curMin=min(price, curMin)
+#             leftMin.append(curMin)
         
-        rightMax=[]
-        curMax=prices[-1]
-        for price in reversed(prices):
-            curMax=max(price, curMax)
-            rightMax.append(curMax)
-        rightMax.reverse()
-        maxProfit=0
-        for i,j in zip(rightMax, leftMin):
-            maxProfit=max(maxProfit, i-j)
-        return maxProfit
+#         rightMax=[]
+#         curMax=prices[-1]
+#         for price in reversed(prices):
+#             curMax=max(price, curMax)
+#             rightMax.append(curMax)
+#         rightMax.reverse()
+#         maxProfit=0
+#         for i,j in zip(rightMax, leftMin):
+#             maxProfit=max(maxProfit, i-j)
+#         return maxProfit
             
+        #intuition: two pointers from left to right
+        if len(prices)==1:
+            return 0
+        left=0
+        right=1
+        maxProfit=0
+        while right<len(prices):
+            maxProfit=max(maxProfit, prices[right]-prices[left])
+            if prices[right]<prices[left]:
+                left=right
+                right+=1
+            else:
+                right+=1
+        return maxProfit
+                
+        
          
     
