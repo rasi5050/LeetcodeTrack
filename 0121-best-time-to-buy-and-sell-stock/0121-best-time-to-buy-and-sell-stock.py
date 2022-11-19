90ff2ee92886b24cc0e9a88d113f5c988e79e7f9
Rasi <rasi5050@gmail.com>
2022-11-19 07:23:43 -0500

    Time: 2998 ms (14.99%), Space: 25.1 MB (39.10%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -19,7 +19,7 @@ class Solution:
 #             maxProfit=max(maxProfit, i-j)
 #         return maxProfit
             
-        #intuition: two pointers from left to right
+        #intuition: two pointers from left to right (idea from: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/1735550/Python-Javascript-Easy-solution-with-very-clear-Explanation)
         if len(prices)==1:
             return 0
         left=0
@@ -33,7 +33,14 @@ class Solution:
             else:
                 right+=1
         return maxProfit
-                
+    
+    #status: success
+    #analysis:Time O(n), going tru input exactly once
+    #Space O(1), 3 variables used, hence O(1)
+    #ref:11/19/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day42/42:(P1:doPaypalLeetcodeQuestion2/5(https://github.com/hxu296/leetcode-company-wise-problems-2022/blob/main/companies/Paypal.csv),do,1.bestTimeToBuyAndSellStocksTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.lruCacheTimed25Min-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.numberOfIslandsTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00)=x9pomo(5:30-10:00)
+
+        
+        
         
          
     
