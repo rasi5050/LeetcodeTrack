3be0721347265f643c3c412dc4206e301dc695bb
Rasi <rasi5050@gmail.com>
2022-11-22 08:36:34 -0500

    Time: 254 ms (85.42%), Space: 16.1 MB (80.53%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        #intuition: utilizing spliting to -ve, +ve, then merge, then square
         #find pivot
         pivot=0
         for i in range(len(nums)):
@@ -7,14 +8,10 @@ class Solution:
                 pivot=i
                 break
         
-        #split by pivot
+        #split by pivot, a has -ve numbers, b has +ve numbers
         a,b=nums[:pivot], nums[pivot:]
-        if nums[pivot]!=abs(nums[pivot]):a=b;b=[]
-        a.reverse()
-        
-        print(a,b)
-        # if abs(b[0])!=b[0]: b.reverse()
-            
+        if nums[pivot]!=abs(nums[pivot]):a=b;b=[]      #if all numbers of -ve, dump b to a
+        a.reverse()     #all are -ve, reverse and square would make in non decreasing order
         a=[i*(-1) for i in a]
         
         #merge + square O(n)
