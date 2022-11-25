0cbdb536a8235452f010b8f7c27b4953e5277325
Rasi <rasi5050@gmail.com>
2022-11-25 09:52:30 -0500

    Time: 217 ms (58.00%), Space: 17.6 MB (11.49%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -35,7 +35,7 @@ class Solution:
         return courseOrder
     """
         
-        #solution from neetcode youtube (https://www.youtube.com/watch?v=Akt3glAwyfY&t=975s)
+        #solution from neetcode youtube: Topological sort (https://www.youtube.com/watch?v=Akt3glAwyfY&t=975s)
         #adjacency list
         preReqMap={ i:[] for i in range(numCourses)}
         for crs, pre in prerequisites:
@@ -61,3 +61,7 @@ class Solution:
             if dfs(c) == False:
                 return []
         return output
+    #status: correct
+    #analysis: Time O(|V|+|E|)
+    #space O(|V|+|E|)
+    #ref: 11/25/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day48/48:1.(P1:doPaypalLeetcodeQuestion6/5;1.4.courseScheduleTimed25Min-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.courseSchedule2TImed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.AllAncestorsOfNodeInADirectedAcyclicGraphTImed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.absorber-x1pomo(10:30-11:00)=x10pomo(6:00-11:00)
