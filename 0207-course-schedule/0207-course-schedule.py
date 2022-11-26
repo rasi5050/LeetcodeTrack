c8389f126a2c45a558bbe339de0c78aab09d907d
Rasi <rasi5050@gmail.com>
2022-11-25 21:24:24 -0500

    Time: 115 ms (82.40%), Space: 17.3 MB (18.65%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,35 +1,27 @@
 from collections import defaultdict
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
-        #create mapping
-        courseMapping = defaultdict(set)
-        for a,b in prerequisites:
-            courseMapping[a].add(b)
-        visited=set()
-        
-        #dfs code
-        #it will get a set, process the set
-        def dfs(mapSet):
-            for each in mapSet:
-                if each in visited:
+
+
+        preReqMapping=defaultdict(set)
+        for preReq in prerequisites:
+            preReqMapping[preReq[0]].add(preReq[1])
+            
+        def dfs(preReq):
+            for eachMap in preReq:
+                if eachMap in currentChain:
                     return False
-                visited.add(each)
-                if not dfs(courseMapping[each]):
+                currentChain.add(eachMap)
+                if eachMap in preReqMapping and dfs(preReqMapping[eachMap])==False:
                     return False
-                visited.remove(each)
-                del courseMapping[each]
-            return True
+                currentChain.remove(eachMap)
         
-        #for each node apply dfs
-        for course in courseMapping.copy().keys():
-            visited.add(course)
-            if not dfs(courseMapping[course]):return False
-            visited.remove(course)
-            del courseMapping[course]
-        return True     
-    
-    #status: correct, 2nd Day try
-    #analysis: Time: O(|V|+|E|), each vertex and edge is visited once
-    #Space: O(|V|+|E|), each vertex and edge is stored
-    #ref:11/25/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day48/48:1.(P1:doPaypalLeetcodeQuestion6/5;1.4.courseScheduleTimed25Min-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.courseSchedule2TImed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.AllAncestorsOfNodeInADirectedAcyclicGraphTImed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.absorber-x1pomo(10:30-11:00)=x10pomo(6:00-11:00)
+        # for course in range(1):
+        for course in range(numCourses):
+            currentChain={course}
+            if course in preReqMapping and dfs(preReqMapping[course])==False:
+                print(currentChain, "hit False for ", course)
+                return False 
+            if course in preReqMapping: del preReqMapping[course]
+            print(currentChain)
+        return True
