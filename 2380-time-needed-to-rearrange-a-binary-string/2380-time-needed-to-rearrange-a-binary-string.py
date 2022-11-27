89cb4a21f53c2df8fcf124735796d508cce6749f
Rasi <rasi5050@gmail.com>
2022-11-27 10:33:13 -0500

    Time: 4982 ms (11.00%), Space: 13.8 MB (71.73%) - LeetHub

---
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -0,0 +1,16 @@
+class Solution:
+    def secondsToRemoveOccurrences(self, s: str) -> int:
+        #loop sliding window, then replace
+        s=list(s)
+        counter=0
+        lenS=len(s)
+        while counter<lenS:
+            #for occurances of zero check '01'
+            switch=False; i=0
+            while i<lenS-1:
+                if s[i:i+2]==['0','1']: s[i:i+2]=['1','0'] ;i+=2; switch=True
+                else:
+                    i+=1
+            if switch==False: return counter
+            counter+=1
+        return counter
