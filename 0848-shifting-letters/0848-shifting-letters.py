c8d677e3f66f51651bcdabb4c36f787c043b8e96
Rasi <rasi5050@gmail.com>
2022-11-27 17:50:47 -0500

    Time: 2321 ms (27.86%), Space: 25 MB (95.34%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -6,10 +6,11 @@ class Solution:
         for i in reversed(range(len(shifts))):
             shifts[i]=(shifts[i]+cumPrev)%26
             cumPrev=shifts[i]
-            if ord(s[i])+shifts[i]>122:
-                s[i]=chr(96+(ord(s[i])+shifts[i])%122)
-            else:
-                s[i]=chr((ord(s[i])+shifts[i]))
+            s[i]=chr((ord(s[i])-97+shifts[i])%26+97)
+            # if ord(s[i])+shifts[i]>122:
+            #     s[i]=chr(96+(ord(s[i])+shifts[i])%122)
+            # else:
+            #     s[i]=chr((ord(s[i])+shifts[i]))
             
         # print(shifts)
         return ''.join(s)
