3338a8d31e6e02802a7fed95de1d9e77e7fea23a
Rasi <rasi5050@gmail.com>
2022-11-27 15:27:58 -0500

    Time: 4629 ms (11.56%), Space: 13.8 MB (71.98%) - LeetHub

--- 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
+++ 2380-time-needed-to-rearrange-a-binary-string/2380-time-needed-to-rearrange-a-binary-string.py
@@ -5,10 +5,10 @@ class Solution:
         counter=0
         lenS=len(s)
         while counter<lenS:
-            #for occurances of zero check '01'
+            #for occurances of zero, check '01'
             switch=False; i=0
             while i<lenS-1:
-                if s[i:i+2]==['0','1']: s[i:i+2]=['1','0'] ;i+=2; switch=True
+                if s[i:i+2]==['0','1']: s[i:i+2]=['1','0']; i+=2; switch=True
                 else:
                     i+=1
             if switch==False: return counter
