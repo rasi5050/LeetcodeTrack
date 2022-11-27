9a0c2c4136f669beec4d8fd3e3bb9aaaba19d68d
Rasi <rasi5050@gmail.com>
2022-11-27 09:28:19 -0500

    Time: 852 ms (48.86%), Space: 28.7 MB (95.84%) - LeetHub

--- 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -1,5 +1,7 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        
+        """#working code ,commented for optimization 
         #sort, find min difference
         arr.sort()
         res=[]
@@ -10,6 +12,23 @@ class Solution:
         #sliding window, only nearest elements after sorting can have min absolute val
         for i in range(len(arr)-1):
             if arr[i+1]-arr[i]==minAbsDiff:
+                res.append([arr[i],arr[i+1]])
+        return res
+        """
+        #status: correct
+        #analysis: Time: O(nlogn + 2n)=O(nLogn), Space: O(1)
+        
+        #optimization: do in one pass Time: O(nLogn + n), Space:O(1) 
+        
+        #sort, find min difference
+        arr.sort()
+        res=[]
+        minAbsDiff=float("+inf")
+        for i in range(len(arr)-1):
+            if arr[i+1]-arr[i] < minAbsDiff:
+                minAbsDiff=arr[i+1]-arr[i]
+                res=[[arr[i],arr[i+1]]]
+            elif arr[i+1]-arr[i]==minAbsDiff:
                 res.append([arr[i],arr[i+1]])
         return res
         
