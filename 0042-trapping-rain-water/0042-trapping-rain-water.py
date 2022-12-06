7915bcc1b72ddb40a5d0509fe77955b54402e94c
Rasi <rasi5050@gmail.com>
2022-12-06 11:20:25 -0500

    Time: 227 ms (69.41%), Space: 16 MB (81.77%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxHToLeft=[]
-        leftMax=0
-        maxHToRight=[]
-        rightMax=0
-        for h in height:
-            maxHToLeft.append(leftMax)
-            leftMax=max(leftMax, h)
+#         maxHToLeft=[]
+#         leftMax=0
+#         maxHToRight=[]
+#         rightMax=0
+#         for h in height:
+#             maxHToLeft.append(leftMax)
+#             leftMax=max(leftMax, h)
             
-        for h in reversed(height):
-            maxHToRight.append(rightMax)
-            rightMax=max(rightMax, h)
-        maxHToRight.reverse()
+#         for h in reversed(height):
+#             maxHToRight.append(rightMax)
+#             rightMax=max(rightMax, h)
+#         maxHToRight.reverse()
+#         res=0
+#         for i,h in enumerate(height):
+#             res+=max(min(maxHToLeft[i], maxHToRight[i])-h,0)
+#         return res
+    
+        #idea2: two pointers, trapping is limited by the lowest height, then increment that side pointer.
+        l,r=0,len(height)-1
+        leftMax,rightMax=height[l], height[r]
         res=0
-        for i,h in enumerate(height):
-            res+=max(min(maxHToLeft[i], maxHToRight[i])-h,0)
+        while l<r:
+            if leftMax<rightMax:
+                l+=1
+                leftMax=max(leftMax, height[l])
+                res+=leftMax-height[l]
+            else:
+                r-=1
+                rightMax=max(rightMax, height[r])
+                res+=rightMax-height[r]
         return res
-            
