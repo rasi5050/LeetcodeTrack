be319a7faa2a2f926435086f2a722671ef82dda9
Rasi <rasi5050@gmail.com>
2022-12-06 09:40:06 -0500

    Time: 2390 ms (54.02%), Space: 28.5 MB (61.69%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -10,7 +10,9 @@ class Solution:
         #             break
         # return res
     #status:TLE, 
-    #idea from leetcode discussion solutions: use decreasing monotonic stack appraoch(https://leetcode.com/problems/daily-temperatures/discuss/1574808/C%2B%2BPython-3-Simple-Solutions-w-Explanation-Examples-and-Images-or-2-Monotonic-Stack-Approaches)
+    #idea from neetcode youtube solution: half help: (https://www.youtube.com/watch?v=cTBiBSnjO3c&t=710s)
+    
+    
     
         stack=[(0,temperatures[0])]
         cur=1
@@ -22,6 +24,11 @@ class Solution:
             stack.append((cur, temperatures[cur]))
             cur+=1
         return res
+        # status: correct
+        #analyis: Time: O(n), each element visited once
+        #Sapce: O(n), since using stack to store intermediate results
+        #ref:12/6/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day52/52-connectMacChargerBeforeTurningOnInMorning:1.calculatorCopyAnswer(https://www.google.com/search?q=calculator+leetcode&rlz=1C5CHFA_enUS1021US1021&oq=calculator+leetcode&aqs=chrome..69i57.2208j0j4&sourceid=chrome&ie=UTF-8#fpstate=ive&vld=cid:aff25d29,vid:zsJ-J08Qgdk)Video-x1pomo(6:00-6:30),2.copyAnswer-x1pomo(6:30-7:00),3.dailyTemperaturesTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),3.trappingRainWaterTimed25Mins-x1pomo(8:30-9:00),4.implement-x2pomo(9:00-10:00),tuSimpleDay1/4D-1/5Q,1.longestIncreasingSubsequenceTimed25Mins-x1pomo(10:00-10:30),2.implement-x2pomo(10:30-11:30)=x11pomo(6:00-11:30)
+
                 
             
             
