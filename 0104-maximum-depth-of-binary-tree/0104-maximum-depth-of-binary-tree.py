cd84419b48be0e03e821b2a78ddec6ff09d0c1c8
Rasi <rasi5050@gmail.com>
2022-12-17 08:50:24 -0500

    Time: 47 ms (85.63%), Space: 15.3 MB (91.11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -18,7 +18,6 @@ class Solution:
         #ref: 12/17/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day53/54,1.largestRectangleInHistogramTImed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.study:tree-x1pomo(7:30-8:00),4.maximumDepthOfABinaryTreeTimed25Mins-x1pomo(8:00-8:30),5.implement-x2pomo(8:30-9:30),6.invert/flipBinaryTreeTimed25Mins-x1pomo(9:30-10:00),7.implement-x2pomo(10:00-11:00),8.lowestCommonAncestorOfABinaryTreeTimed25Mins-x1pomo(10:00-10:30),9.absorber-x1pomo(10:30-11:00)=x12pomo(6:00-12:00)
         
         #iterative approach: use bfs, number of levels will be max height of tree
-        # print(root)
         frontier=[]
         if root: frontier.append(root)
         level=0
@@ -26,9 +25,9 @@ class Solution:
             next=[]
             level+=1
             for curNode in frontier:
-                if curNode and curNode.left: next.append(curNode.left)
-                if curNode and curNode.right: next.append(curNode.right)
-            frontier=next[:]
+                if curNode.left: next.append(curNode.left)
+                if curNode.right: next.append(curNode.right)
+            frontier=next
         return level
         
         
