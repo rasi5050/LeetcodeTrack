bb9783a00ca34ef923c423aac6cadf06a643fd79
Rasi <rasi5050@gmail.com>
2022-12-18 13:01:08 -0500

    Time: 70 ms (38.07%), Space: 14.1 MB (84.25%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -19,3 +19,6 @@ class Solution:
             frontier=next[:]
             res.append(temp)
         return res
+    #status: correct
+    #analysis: Time O(n), Space O(n)
+    #ref: 12/18/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day54/54,1.binaryTreeMaximumPathSumTimed25Mins-x1pomo(7:00-7:30),2.implement-x2pomo(7:30-8:30)=x3pomo(7:00-8:30)
