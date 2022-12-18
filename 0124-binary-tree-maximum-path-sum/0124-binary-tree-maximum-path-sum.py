b4ce325da7ae10d9911e0d0cc30988268a9ba835
Rasi <rasi5050@gmail.com>
2022-12-18 12:40:58 -0500

    Time: 117 ms (70.01%), Space: 21.3 MB (63.23%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -6,9 +6,9 @@
 #         self.right = right
 class Solution:
     def  maxPathSum(self, root: Optional[TreeNode]) -> int:
+        #intuition from neetcode youtube solution (https://www.youtube.com/watch?v=Hr5cWUld4vU&t=1s)
         res=[root.val]
-        def recurse(root):
-            
+        def recurse(root): 
             if not root:
                 return 0
             l=max(recurse(root.left),0)
@@ -17,3 +17,7 @@ class Solution:
             return root.val + max(l, r)
         recurse(root)
         return res[0]
+    
+    #status: correct
+    #analysis: Time O(n), Space O(1)
+    #ref: 12/18/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day54/54,1.binaryTreeMaximumPathSumTimed25Mins-x1pomo(7:00-7:30),2.implement-x2pomo(7:30-8:30)=x3pomo(7:00-8:30)
