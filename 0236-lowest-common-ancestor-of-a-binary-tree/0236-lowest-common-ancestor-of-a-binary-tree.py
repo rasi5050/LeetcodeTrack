d12863157bee2173c8b4dbae403c0498eafc6a34
Rasi <rasi5050@gmail.com>
2022-12-19 13:07:44 -0500

    Time: 206 ms (13.31%), Space: 26.3 MB (65.09%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -16,3 +16,8 @@ class Solution:
         elif left: return left
         else: return right
         
+        #status: success: help from https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/1306476/C%2B%2BPython-2-solutions-Clean-and-Concise-O(N)
+        #analysis: Time O(n); visiting all nodes, Space O(1); nothing stored
+        #ref: 12/19/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day55/56,1.lowestCommonAncestorOfABinaryTreeTimed25Mins-x1pomo(10:00-10:30),2.implement-x2pomo(10:30-11:30)-x3pomo(10:00-11:30)
+
+        
