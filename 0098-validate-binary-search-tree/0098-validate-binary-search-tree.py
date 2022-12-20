09c7d1eae3696448d26b43d149f87eda3851c2d1
Rasi <rasi5050@gmail.com>
2022-12-20 10:19:33 -0500

    Time: 105 ms (18.10%), Space: 17.6 MB (13.80%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -16,3 +16,6 @@ class Solution:
         dfs(root)
         return sorted(set(res))==res
     
+    #status: correct
+    #analysis: Time O(n logn); involves sorting, Space O(n); storing the values in list
+    #ref: 12/20/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day56/56,1.constructBinaryTreeFromPreorderAndInorderTraversalTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.serializeAndDeserializeBinaryTreeTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.validateBinarySearchTreeTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.kThSmallestElementInABstTimed25Mins-x1pomo(10:00-10:30),8.implement-x2pomo(10:30-11:30)=x12pomo(5:30-11:30)
