5bdb7dea240a862b14c60d07055b0cd74ab3d6d6
Rasi <rasi5050@gmail.com>
2022-12-20 12:13:24 -0500

    Time: 98 ms (32.22%), Space: 16.6 MB (27.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -35,7 +35,7 @@ class Solution:
         return dfs(root)==True
         """
         #optimize sort code for O(n)
-        
+        """ working code
         res=[]
         def dfs(node):
             if not node : return    
@@ -46,7 +46,16 @@ class Solution:
         for i in range(len(res)-1):      #O(n)
             if res[i]>=res[i+1]: return False   
         return True
-        
+        """
         #status: correct; improved runtime complexity
         #Analysis: Time O(n); only adjacent elements has to be checked for sorted, Space O(n)
         #ref: #ref: 12/20/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day56/56,1.constructBinaryTreeFromPreorderAndInorderTraversalTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.serializeAndDeserializeBinaryTreeTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.validateBinarySearchTreeTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.kThSmallestElementInABstTimed25Mins-x1pomo(10:00-10:30),8.implement-x2pomo(10:30-11:30)=x12pomo(5:30-11:30)
+        def checker(root, lower, upper):
+            if not root: 
+                return True
+            if lower>=root.val or upper<= root.val:
+                return False
+            else:
+                return checker(root.left, lower, root.val) and checker(root.right, root.val, upper)
+        return checker(root, -math.inf, math.inf)
+        
