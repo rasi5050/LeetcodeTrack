8f93eb8516a67f0336d00a000cedb68be1578082
Rasi <rasi5050@gmail.com>
2022-12-21 10:48:15 -0500

    Time: 225 ms (5.10%), Space: 14 MB (90.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -22,7 +22,7 @@ class Solution:
     #analysis: Time O(n); might have to color all elements if all connected to it
     #Space O(1); not storing anything extra
     #ref: 12/21/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day57/58,1.kThSmallestElementInABstTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.study:graph-x1pomo(7:00-7:30),4.numberOfIslandsTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.floodFillTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.01MatrixTimed25Mins-x1pomo(10:30-11:00),9.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
-        
+        #bfs
         fromColor=image[sr][sc]
         if fromColor==color: return image
         frontier=[(sr,sc)]
@@ -38,5 +38,6 @@ class Solution:
                     next.append((i, j+1))
             frontier=next[:]
         return image
+    
     
     
