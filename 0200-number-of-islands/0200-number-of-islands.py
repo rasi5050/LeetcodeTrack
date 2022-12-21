58808534e07034d7458b609524a5846a160026a0
Rasi <rasi5050@gmail.com>
2022-12-21 09:01:15 -0500

    Time: 278 ms (98.26%), Space: 16.2 MB (92.30%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,20 +6,17 @@ class Solution:
         for m in range(len(grid)):
             for n in range(len(grid[0])):
                 if grid[m][n]=='1':
-                    # print(grid)
                     noOfIslands+=1
                     frontier=[(m,n)]
                     while frontier:
                         next=[]
                         for index in frontier:
                             i, j = index
-                            # print(index)
                             if grid[i][j] == '1':
                                 grid[i][j] = '-1'
-                                if 0<i and grid[i-1][j]=='1'            : next.append((i-1,j))
-                                if i<len(grid)-1 and grid[i+1][j]=='1'     : next.append((i+1,j))
-                                if 0<j and grid[i][j-1]=='1'           : next.append((i,j-1))
-                                if j<len(grid[0])-1 and grid[i][j+1]=='1'  : next.append((i,j+1))    
+                                if 0<i and grid[i-1][j]=='1'                : next.append((i-1,j))
+                                if i<len(grid)-1 and grid[i+1][j]=='1'      : next.append((i+1,j))
+                                if 0<j and grid[i][j-1]=='1'                : next.append((i,j-1))
+                                if j<len(grid[0])-1 and grid[i][j+1]=='1'   : next.append((i,j+1))    
                         frontier=next[:]
-        print(grid)
         return noOfIslands
