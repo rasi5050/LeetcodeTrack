d51c00600da59d087172d8d366180b2afd6c6b42
Rasi <rasi5050@gmail.com>
2022-12-21 09:00:04 -0500

    Time: 289 ms (96.44%), Space: 17.2 MB (39.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,34 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        
-        #intuition: apply dfs on all nodes
-        m,n=len(grid), len(grid[0])
-        visited=set()
-        def neighbors(s):
-            x,y=s[0],s[1]
-            res=[(x+1,y) if x+1<m else None,(x-1,y) if x-1>=0 else None,(x,y+1) if y+1<n else None,(x,y-1) if y-1>=0 else None]
-            return [ i for i in res if i]
-        
-        def dfs(s):
-            if s not in visited and grid[s[0]][s[1]]=="1":
-                visited.add(s)
-                for adj in neighbors(s):
-                    dfs(adj)
-        
-        count=0
-        for i in range(m):
-            for j in range(n):
-                
-                l1=len(visited)
-                dfs((i,j))
-                l2=len(visited)
-                if l2>l1:
-                    print(i,j)
-                    count+=1
-        return count
-    #status: correct
-    #analysis: Time O(m*n), its visiting all elements to make decision
-    # Space O(m*n): it stores visited set()
-    #ref:11/21/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day44/44:1.doOnSunday:mathWorks-lastDate21 Nov 2022 10:39 AM EST (America - New York)-105MinutesAsperHackerRank(90Mins,expected60MinsAsPerMathworks):doOn11/21/2022-x3pomo(5:30-7:00),2.addQuestionsToDrive-x2pomo(7:00-8:00),3.(P1:doPaypalLeetcodeQuestion2/5(https://github.com/hxu296/leetcode-company-wise-problems-2022/blob/main/companies/Paypal.csv),do,4.lruCacheTimed25Min-x1pomo(8:00-8:30),5.implement-x2pomo(8:30-9:30),6.numberOfIslandsTimed25Mins-x1pomo(9:30-10:00),7.implement-x2pomo(10:00-11:00)=x11pomo(5:30-11:00)
-
-    
+        #do BFS
+        #fill frontier using foor loop to all elements, if element is 1
+        noOfIslands=0
+        for m in range(len(grid)):
+            for n in range(len(grid[0])):
+                if grid[m][n]=='1':
+                    # print(grid)
+                    noOfIslands+=1
+                    frontier=[(m,n)]
+                    while frontier:
+                        next=[]
+                        for index in frontier:
+                            i, j = index
+                            # print(index)
+                            if grid[i][j] == '1':
+                                grid[i][j] = '-1'
+                                if 0<i and grid[i-1][j]=='1'            : next.append((i-1,j))
+                                if i<len(grid)-1 and grid[i+1][j]=='1'     : next.append((i+1,j))
+                                if 0<j and grid[i][j-1]=='1'           : next.append((i,j-1))
+                                if j<len(grid[0])-1 and grid[i][j+1]=='1'  : next.append((i,j+1))    
+                        frontier=next[:]
+        print(grid)
+        return noOfIslands
