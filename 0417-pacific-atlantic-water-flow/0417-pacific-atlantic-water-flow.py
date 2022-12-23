31d4359a6a4c4aff9a8c64c9e0d145e29e3f3cec
Rasi <rasi5050@gmail.com>
2022-12-23 10:52:39 -0500

    Time: 382 ms (74.32%), Space: 15.5 MB (79.88%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -9,7 +9,6 @@ class Solution:
                 next=[]
                 for nei in frontier:
                     i,j = nei
-                    # print(nei)
                     if isVisited[i][j]: continue
                     isVisited[i][j]=True
     
@@ -33,5 +32,7 @@ class Solution:
             for j in range(len(heights[0])):
                 if canVisitA[i][j] and canVisitP[i][j]: res.append([i, j])
         return res
-                
-        
+        #status: correct; @Rasi's code ;translated from c++ bfs solution (https://leetcode.com/problems/pacific-atlantic-water-flow/discuss/1126938/Short-and-Easy-w-Explanation-and-diagrams-or-Simple-Graph-traversals-DFS-and-BFS)
+        #Analyis: Time O(mn);  cells can be visited by both atlantic and pacific oceans
+        #Space: O(3*mn)=O(mn); visited is kept track in different matrix
+        #ref: 12/23/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day59/60,1.cloneGraphTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.pacificAtlanticWaterFlowTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.courseScheduleTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.studyHeap-x1pomo(10:00-10:30),8.mergeKSortedListsTimed25Mins-x1pomo(10:30-11:00),9.implement-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
