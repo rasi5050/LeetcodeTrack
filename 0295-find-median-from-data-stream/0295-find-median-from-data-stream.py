72f547d4bece5fb2f27abd3b7bfc4ecf15910b66
Rasi <rasi5050@gmail.com>
2022-12-24 11:31:01 -0500

    Time: 546 ms (94.66%), Space: 35.7 MB (81.09%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -46,4 +46,9 @@ class MedianFinder:
         maxHeap, minHeap = self.maxHeap, self.minHeap
         if len(maxHeap) > len(minHeap): return -maxHeap[0]
         else: return (-maxHeap[0] + minHeap[0])/2
-            
+    
+    #status: success; idea from (https://leetcode.com/problems/find-median-from-data-stream/discuss/2805054/python3-Max-and-Min-heaps-oror-SortedList-oror-O(log-n))
+    #Analaysis: Time O(log n); limited by O(heappush or heappop)
+    #Space O(n); total elements stored in heap
+    #ref: 12/24/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day60/60,1.studyHeap-x1pomo(5:30-6:00),2.mergeKSortedListsTimed25Mins-x1pomo(6:00-6:30),3.implement-x1pomo(6:30-7:30),4.kSmallestPointsToOriginTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.topKFrequentElementsTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.findMedianFromDataStreamTimed25Mins-x1pomo(10:30-11:00),9.implement-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
+
