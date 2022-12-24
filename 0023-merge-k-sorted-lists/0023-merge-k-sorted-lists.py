02c9dc662e2cedd8110e64435dddb822f6a80b37
Rasi <rasi5050@gmail.com>
2022-12-24 07:28:29 -0500

    Time: 107 ms (91.09%), Space: 18.4 MB (19.67%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -16,5 +16,9 @@ class Solution:
             carrier.next=ListNode(heapq.heappop(res))
             carrier=carrier.next
         return root.next
-                
+    #status: correct
+    #Analysis: TimeO(nlogn), heappush is logn, that is repeated number of nodes time, ie. n, hence n logn
+    #Space O(n): n; storing all elements in res[]
+    #ref: 12/24/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day60/60,1.studyHeap-x1pomo(5:30-6:00),2.mergeKSortedListsTimed25Mins-x1pomo(6:00-6:30),3.implement-x1pomo(6:30-7:30),4.kSmallestPointsToOriginTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.topKFrequentElementsTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.findMedianFromDataStreamTimed25Mins-x1pomo(10:30-11:00),9.implement-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
+
         
