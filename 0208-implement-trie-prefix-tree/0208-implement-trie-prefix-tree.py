e0547f4499cc91b1df68d03f6749db4c176c6478
Rasi <rasi5050@gmail.com>
2022-12-25 08:53:50 -0500

    Time: 180 ms (87.58%), Space: 31.4 MB (73.44%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -51,5 +51,5 @@ class Trie:
 
 #status: correct; help from comments to leetcode article on trie of user:"inawhsa" (Feb 28, 2021); (https://leetcode.com/problems/implement-trie-prefix-tree/solutions/127843/official-solution/)
 #Analysis: Time; insert O(m), where m is the word length, search O(m), search_prefix O(m)
-#Space; insert O(m); word might not be present, search O(m); word might be present, search_prefix O(m); prefix might be present
+#Space; insert O(m); word might not be present, search O(1); no space requirement for search, search_prefix O(1); no space requirement for search_prefix
 #ref: 12/25/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day61/62,1.studyTrie-x1pomo(5:30-6:00),2.implementTrieTimed25Mins-x1pomo(6:00-6:30),3.implement-x2pomo(6:30-7:30),4.addAndSearchWorkdTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.wordBreakTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.wordSearch2Timed25Mins-x1pomo(10:30-11:00),9.implement-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
