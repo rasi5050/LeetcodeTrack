08e20f336a491931ae42f7ea9b0c0ee842b3d090
Rasi <rasi5050@gmail.com>
2022-12-25 12:14:17 -0500

    Time: 10307 ms (52.73%), Space: 78 MB (47.60%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -74,4 +74,8 @@ class WordDictionary:
                     cur=cur.children[c]
             return cur.word
         return dfs(0, self.root)
-        
+        #status: correct; blind copy from neetcode youtube solution (https://www.youtube.com/watch?v=BTf05gs_8iU&t=1163s)
+        #improvements in video comments: user "Lulit999" comments to video; he also given it in leetcode comments (https://leetcode.com/problems/design-add-and-search-words-data-structure/discuss/2622355/Do-you-have-TLE-Add-this-one-thing-to-your-code!) 
+        #Analysis: Time O(m*n); for a word all the tree might have to be recursed
+        #Space O(m*n); n words of size m stored
+        #ref: 12/25/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day61/62,1.studyTrie-x1pomo(5:30-6:00),2.implementTrieTimed25Mins-x1pomo(6:00-6:30),3.implement-x2pomo(6:30-7:30),4.addAndSearchWorkdTimed25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.wordBreakTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.wordSearch2Timed25Mins-x1pomo(10:30-11:00),9.implement-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
