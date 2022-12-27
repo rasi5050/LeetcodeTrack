5de5792ed78570b2a8e00fd159c5a1c7edc7071a
Rasi <rasi5050@gmail.com>
2022-12-27 09:41:39 -0500

    Time: 147 ms (95.09%), Space: 18.2 MB (39.60%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -14,6 +14,10 @@ class Solution:
                 res.append(intervals[i])
         res.append(intervals[-1])
         return res
-                
+        #status: correct; partial help from overlap and merging methods of https://www.techinterviewhandbook.org/algorithms/interval/
+        #Analysis: Time O(n); length of intervals
+        #Space O(n); all the intervals might be unique
+        #ref: 12/27/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day64/64,1.wordSearch2Timed25Mins-x1pomo(5:30-6:00),2.implement-x1pomo(6:00-7:00),3.studyIntervals-x1pomo(7:00-7:30),4.mergeIntervalsTimed;25Mins-x1pomo(7:30-8:00),5.implement-x2pomo(8:00-9:00),6.insertIntervalTImed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.nonOverlappingIntervalsTimed25Mins-x1pomo(10:30-11:00),9.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
+
                 
             
