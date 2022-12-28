b0f58998639583dadd31fa82b3c5656985d7446b
Rasi <rasi5050@gmail.com>
2022-12-28 07:58:12 -0500

    Time: 24 ms (98.53%), Space: 13.8 MB (57.51%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        #say n=5
+        memo={}
+        def recurse(i):
+            if i in memo: return memo[i]
+            if i<0: return 0
+            elif i==0: return 1
+            memo[i]=recurse(i-1) + recurse(i-2)
+            return memo[i]
+        return recurse(n)
