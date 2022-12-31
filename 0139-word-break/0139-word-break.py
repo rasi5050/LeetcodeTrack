a1f7c5d4f14c3949fa98b168ae086dbcadb71e7f
Rasi <rasi5050@gmail.com>
2022-12-31 07:46:49 -0500

    Time: 44 ms (80.59%), Space: 13.9 MB (94.96%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,54 +1,37 @@
+"""
 class TreeNode:
     def __init__(self):
         self.children={}
-        self.word=False
+        self.isWord=False
+"""
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        """wrong solution: attempted using tries; dont know to implement case
-        #"aaaaaaa"
-        #["aaaa","aaa"]
-        root=node=TreeNode()
-        lvl=0
-        #insert word into prefix tree
-        def insert(wordDict):
-            nonlocal lvl
-            nonlocal node
-            for word in wordDict:
-                for c in word:
-                    if c in node.children:
-                        node=node.children[c]
-                    else:
-                        new_node=TreeNode()
-                        node.children[c]=new_node
-                        node=new_node
-                node.word=True
-                node=root
-        insert(wordDict)
-    
-        #search in prefix trie
-        #if it was only leet, how would you do?
-        def search(j):
-            node=root
-            for i in range(j, len(s)):
-                if s[i] not in node.children:
-                    return False
-                node=node.children[s[i]]
-                if node.word and i<len(s)-1:
-                    print('hit@', i)
-                    return search(i+1)
-            return node.word
-        return search(0)
         """
-        # solution form neetcode youtube video (https://www.youtube.com/watch?v=Sx9NNgInc3A&t=5s)
-        dp = [False] * (len(s)+1)        
-        dp[len(s)] = True #base case
-        for i in range(len(s) -1, -1, -1): #equivalent to last element to first element
-            for w in wordDict:
-                if (i+len(w)) <= len(s) and s[i: i + len(w)] == w:
-                    dp[i]=dp[i + len(w)]
+        #create Trie node for the list of words
+        root=node=TreeNode()
+        for word in wordDict:
+            node=root
+            for c in word:
+                if c not in node.children:
+                    new_node=TreeNode()
+                    node.children[c]=new_node
+                node=node.children[c]
+            node.isWord=True
+        """
+        #i remember this question had edge case of 'aaa', 'aaaa', to be matched with 'aaaaaaa', linear searching will only create 'aaa' 2 times and hence fails to create 'a' of 7 times
+        
+        #correlated to one of the answers
+        
+        #better approach in neetcode solution:
+        
+        dp = [False] * (len(s)+1)
+        dp[len(s)] = True
+        
+        for i in range(len(s)-1, -1, -1):
+            for word in wordDict:
+                if (i+len(word)) <= len(s) and s[i: i+len(word)]==word:
+                    dp[i] = dp[i + len(word)]
                 if dp[i]:
                     break
-        return dp[0]
-                        
-    
+        return dp[0] 
         
