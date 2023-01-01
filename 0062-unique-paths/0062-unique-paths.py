2575613f1ca52359fe640b9f850a707137c1383b
Rasi <rasi5050@gmail.com>
2023-01-01 08:58:53 -0500

    Time: 39 ms (69.62%), Space: 13.9 MB (73.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -17,6 +17,7 @@ class Solution:
             return memo[(x+1, y, m, n)] + memo[(x, y+1, m, n)]
         return recurse(0, 0, m, n)
         """
+        """correct answer, commented to try alternate method
         #do reverse
         memo={}
         def recurse(x, y):
@@ -30,3 +31,14 @@ class Solution:
             memo[(x, y-1)] = recurse(x, y-1)
             return memo[(x-1, y)] + memo[(x, y-1)]
         return recurse(m-1, n-1)
+        """
+        dp=[[0 for _ in range(n+1)] for _ in range(m+1)]
+        dp[0][1]=1
+        for i in range(1,m+1):
+            for j in range(1,n+1):
+                dp[i][j] = dp[i][j-1] + dp[i-1][j] 
+        print(dp)
+        return dp[m][n]
+    
+    
+        
