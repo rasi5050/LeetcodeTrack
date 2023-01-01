80a26b569b141b0af5ec1f5619e42685c272ff17
Rasi <rasi5050@gmail.com>
2023-01-01 08:30:15 -0500

    Time: 32 ms (88.80%), Space: 14.2 MB (6.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,6 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         #do recursively
+        """correct code; commented for alternate method
         m,n=m-1,n-1
         memo = {}
         def recurse(x, y, m, n):
@@ -15,3 +16,17 @@ class Solution:
             
             return memo[(x+1, y, m, n)] + memo[(x, y+1, m, n)]
         return recurse(0, 0, m, n)
+        """
+        #do reverse
+        memo={}
+        def recurse(x, y):
+            if (x,y) in memo:
+                return memo[(x,y)]
+            if (x,y)==(0,0):
+                return 1
+            elif x<0 or y<0:
+                return 0
+            memo[(x-1, y)] = recurse(x-1, y) 
+            memo[(x, y-1)] = recurse(x, y-1)
+            return memo[(x-1, y)] + memo[(x, y-1)]
+        return recurse(m-1, n-1)
