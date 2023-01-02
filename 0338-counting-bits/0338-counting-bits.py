d54bb1ef122fa157a1d6c581fe65b2f676fde497
Rasi <rasi5050@gmail.com>
2023-01-02 11:29:49 -0500

    Time: 103 ms (76.21%), Space: 25.8 MB (28.71%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -15,7 +15,7 @@ class Solution:
     #Analysis: O(n)*O(n)=O(n^2), Space O(n)
     #ref: 1/2/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day70/70,0.applyForOncampusJobEventsAndTechServices,1.sumOfTwoIntegersTimed25mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.numberOf1BitsTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.countingBitsTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.missingNumberTimed25Mins-x1pomo(10:00-10:30),8.implement-x2pomo(10:30-11:30)=x12pomo(5:30-11:30)
         """
-        
+        """working code; commented for improvement
         def solve(n):
             #base condition
             if n==0: return 0
@@ -28,4 +28,24 @@ class Solution:
             res.append(solve(i))
         return res
         #Analysis: Time O(nLogn)
-    
+        """
+        
+        #implement recursion + memoization a.k.a dynamic programming
+        memo={}
+        def solve(n):
+            if n in memo: return memo[n] 
+            #base condition
+            if n==0: return 0
+            elif n==1: return 1
+            
+            if n%2: 
+                memo[n] = 1 + solve(n//2)
+                return memo[n]
+            else: 
+                memo[n] = solve(n//2)
+                return memo[n]
+        res=[]
+        for i in range(n+1):
+            res.append(solve(i))
+        return res
+        
