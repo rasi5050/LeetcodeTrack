312876f8607a6f8eb59c97acf1572d317a22620e
Rasi <rasi5050@gmail.com>
2023-01-03 11:00:46 -0500

    Time: 335 ms (40.28%), Space: 15.2 MB (78.53%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -2,8 +2,11 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         #subtract each number from sum of whole series
         n=len(nums)
-        sumSeries = (n*(n+1))//2
-        
+        sumSeries = (n*(n+1))//2        
         for i in nums:
             sumSeries-=i
         return sumSeries
+    #status: correct
+    #Analysis: Time O(n); looping tru each element
+    #Space O(1); only sumSeries
+    #ref: 1/3/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day71/72,1.OA:hudsonRiverTrading-x2pomo(5:30-6:30),2.copyQuestionsToDrive-x2pomo(6:30-7:30),3.missingNumberTimed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.reverseBitsTimed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.singleNumberTimed25Mins-x1pomo(10:30-11:00),8.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)
