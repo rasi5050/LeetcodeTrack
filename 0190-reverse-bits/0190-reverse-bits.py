8c9e35b4837876c1c0a142fb9532d74074b26954
Rasi <rasi5050@gmail.com>
2023-01-04 09:01:37 -0500

    Time: 59 ms (40.31%), Space: 13.8 MB (48.26%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -20,3 +20,7 @@ class Solution:
             n>>=1       #remove the last considered bit form n
 
         return binaryNum
+    
+    #status: correct; idea from (https://leetcode.com/problems/reverse-bits/discuss/2339454/Easiest-Way-to-do-oror-Bit-Manipulation-oror-Java-Code)
+    #Analysis: Time O(32)=O(1); space O(1)
+    #ref: 1/4/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day72/72,1.reverseBitsTimed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.singleNumberTimed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.pow(x,n)Timed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.sqrt(n)Timed25Mins-x1pomo(10:30-11:00),8.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)delayed(6:00-11:30)
