b3d7174d61c73523d3f7fdad6270d7466a7167f7
Rasi <rasi5050@gmail.com>
2023-01-04 15:49:42 -0500

    Time: 87 ms (33.47%), Space: 13.8 MB (55.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -6,6 +6,7 @@ class Solution:
                 return i
         return x
         """
+        """
         #instead of going all the way minimize the work in a range
         t=x
         while t:
@@ -16,3 +17,19 @@ class Solution:
             if (i*i) <= x < (i+1)*(i+1):
                 return i
         return x
+        """
+        #better idea; binary search from in 0 to x//2 range
+        if x==1: return 1
+        l=0
+        r=x//2
+        
+        while l<=r:
+            mid=(l+r)//2
+            prod = mid*mid
+            if prod == x:
+                return mid
+            elif prod>x:
+                r=mid-1
+            else:
+                l=mid+1
+        return r
