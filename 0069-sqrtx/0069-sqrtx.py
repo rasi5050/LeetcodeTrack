f41eb5139401bda742d98ed60a2a0c9e28997108
Rasi <rasi5050@gmail.com>
2023-01-04 16:23:29 -0500

    Time: 33 ms (94.36%), Space: 13.9 MB (55.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -19,6 +19,7 @@ class Solution:
         return x
         """
         #better idea; binary search from in 0 to x//2 range
+        """working code; commented to try alternate method
         if x==1: return 1
         l=0
         r=x//2
@@ -33,3 +34,17 @@ class Solution:
             else:
                 l=mid+1
         return r
+        """
+        if x==1: return 1
+        l, r = 0, x//2
+        while l<=r:
+            mid = (l+r)//2
+            prod = mid*mid
+            prodNext = (mid+1)*(mid+1)
+            if prod <= x < prodNext:
+                return mid
+            elif prod>x:
+                r=mid-1
+            else:
+                l=mid+1
+            
