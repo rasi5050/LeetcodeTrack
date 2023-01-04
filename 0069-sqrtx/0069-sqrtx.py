70b382e8a4b5209cdfaf2f2502f7188f833bda57
Rasi <rasi5050@gmail.com>
2023-01-04 16:24:16 -0500

    Time: 61 ms (60.16%), Space: 13.9 MB (55.01%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,50 +1,11 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        """TLE
-        for i in range(x):
-            if (i*i) <= x < (i+1)*(i+1):
-                return i
-        return x
-        """
-        """
-        #instead of going all the way minimize the work in a range
-        t=x
-        while t:
-            t//=2
-            if t*t<=x:
-                break
-        for i in range(t, x):
-            if (i*i) <= x < (i+1)*(i+1):
-                return i
-        return x
-        """
-        #better idea; binary search from in 0 to x//2 range
-        """working code; commented to try alternate method
-        if x==1: return 1
-        l=0
-        r=x//2
-        
-        while l<=r:
-            mid=(l+r)//2
-            prod = mid*mid
-            if prod == x:
+class Solution(object):
+    def mySqrt(self, x):
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r-l)//2
+            if mid * mid <= x < (mid+1)*(mid+1):
                 return mid
-            elif prod>x:
-                r=mid-1
+            elif x < mid * mid:
+                r = mid - 1
             else:
-                l=mid+1
-        return r
-        """
-        if x==1: return 1
-        l, r = 0, x//2
-        while l<=r:
-            mid = (l+r)//2
-            prod = mid*mid
-            prodNext = (mid+1)*(mid+1)
-            if prod <= x < prodNext:
-                return mid
-            elif prod>x:
-                r=mid-1
-            else:
-                l=mid+1
-            
+                l = mid + 1
