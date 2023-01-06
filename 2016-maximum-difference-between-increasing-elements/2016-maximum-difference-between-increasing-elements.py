75049ff8a6aff19901e9f0a689c65ce3f306656a
Rasi <rasi5050@gmail.com>
2023-01-06 08:00:49 -0500

    Time: 79 ms (65.46%), Space: 14.1 MB (37.40%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.py
@@ -23,6 +23,11 @@ class Solution:
                 curMax=n
                 res = max(res, curMax - curMin)
         return res if res else -1
+    
+        #status: correct
+        #Analysis: Time O(n); one pass algorithm, Space O(1)
+        #ref: 1/6/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day74/74-ciscoDay2/2,1.maximumDifferenceBetweenIncresingElementsTimed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.wordSearch2Timed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.burstBaloonsTimed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.decodeWaysTimde25Mins-x1pomo(10:30-11:00),8.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)delayed(6:00-11:30)
+
         
         
                 
