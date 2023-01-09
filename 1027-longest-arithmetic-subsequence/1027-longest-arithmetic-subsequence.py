177e9b46e37f8e695075cb4035b95b77adea6250
Rasi <rasi5050@gmail.com>
2023-01-09 14:23:05 -0500

    Time: 3689 ms (66.92%), Space: 66.4 MB (18.67%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -6,3 +6,6 @@ class Solution:
                 dp[(j, nums[j]-nums[i])] = dp.get((i,nums[j]-nums[i]), 1) + 1
         return max(dp.values())
                 
+    #status: correct; didnot understand; idea from leetcode discussions solutions (https://leetcode.com/problems/longest-arithmetic-subsequence/discuss/274611/JavaC%2B%2BPython-DP)
+    #Analysis: Time O(n^2), Space O(n^2)
+    #ref:1/9/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day76/76-,1.countVowelsPermutationsTimed25Mins-x1pomo(18:00-18:30),2.implement-x2pomo(18:30-19:30),3.absorber-x1pomo(19:30-20:00)=x4pomo(18:00-20:00)early(12:30-14:30)              
