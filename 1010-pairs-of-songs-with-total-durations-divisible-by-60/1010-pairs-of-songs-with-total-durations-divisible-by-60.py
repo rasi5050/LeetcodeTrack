1eb0ac03902319e72eac7d069473a6645199f9f8
Rasi <rasi5050@gmail.com>
2023-01-10 09:29:57 -0500

    Time: 332 ms (65.12%), Space: 17.2 MB (99.61%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -3,7 +3,6 @@ class Solution:
         #convert problem into 2 sum by modding all value by 60
         for i in range(len(time)):
             time[i]%=60
-        print(time)
         
         map={}
         total=0
