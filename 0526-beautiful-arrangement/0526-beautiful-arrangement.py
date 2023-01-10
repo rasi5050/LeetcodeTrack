5579ea15b3896a5ca456d55dff48d9250f211916
Rasi <rasi5050@gmail.com>
2023-01-10 13:44:11 -0500

    Time: 1603 ms (51.51%), Space: 13.9 MB (73.18%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -5,13 +5,11 @@ class Solution:
         visited=set()
         def recurse(arr):
             if len(arr)==n+1:
-                print(arr)
                 count[0]+=1
                 return
             
             for i in range(1, n+1):
-                if i not in visited and (len(arr)%i==0 or i%len(arr)==0):
-                    
+                if i not in visited and (len(arr)%i==0 or i%len(arr)==0):           
                     visited.add(i)
                     arr.append(i)
                     recurse(arr)
