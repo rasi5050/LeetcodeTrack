464a8027fb96988ad1608fe8254675760170d18c
Rasi <rasi5050@gmail.com>
2023-01-10 13:50:31 -0500

    Time: 1689 ms (49.13%), Space: 14 MB (33.08%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -17,6 +17,22 @@ class Solution:
                     visited.remove(i)
         recurse(['dummy'])
         return count[0]
-    #status: correct
+    #status: correct; idea from (https://www.youtube.com/watch?time_continue=269&v=xf8qAkqDr8Y&feature=emb_logo&themeRefresh=1); @Rasi's Code
     #Analysis: Time O(n^2), Space O(1)
     #ref: 1/10/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day76/76-,1.implement-x2pomo(12:30-13:30),2.Daily TemperaturesTimed25Mins-x1pomo(13:30-14:00),3.absorber-x1pomo(14:00-14:30)=x4pomo(12:30-14:30)
+    
+    #remove count[]
+        visited=set()
+        def recurse(arr):
+            if len(arr)==n+1:
+                return 1
+            count=0
+            for i in range(1, n+1):
+                if i not in visited and (len(arr)%i==0 or i%len(arr)==0):           
+                    visited.add(i)
+                    arr.append(i)
+                    count+=recurse(arr)
+                    arr.pop()
+                    visited.remove(i)
+            return count
+        return recurse(['dummy'])
