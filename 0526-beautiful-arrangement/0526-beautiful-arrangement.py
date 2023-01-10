34777ed399d9b61bb2c27ecb066ff8e399840f92
Rasi <rasi5050@gmail.com>
2023-01-10 14:12:53 -0500

    Time: 1629 ms (51.01%), Space: 13.8 MB (98.37%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -21,8 +21,10 @@ class Solution:
     #Analysis: Time O(n^2), Space O(1)
     #ref: 1/10/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day76/76-,1.implement-x2pomo(12:30-13:30),2.Daily TemperaturesTimed25Mins-x1pomo(13:30-14:00),3.absorber-x1pomo(14:00-14:30)=x4pomo(12:30-14:30)
     """
+
     #remove count[]
         visited=set()
+
         def recurse(arr):
             if len(arr)==n+1:
                 return 1
@@ -37,20 +39,5 @@ class Solution:
             return count
         return recurse(['dummy'])
     
-    """
-    #remove count[]
-        visited=set()
-        def recurse(arr):
-            if len(arr)==n+1:
-                return 1
-            count=0
-            for i in range(1, n+1):
-                if i not in visited and (len(arr)%i==0 or i%len(arr)==0):           
-                    visited.add(i)
-                    arr.append(i)
-                    count+=recurse(arr)
-                    arr.pop()
-                    visited.remove(i)
-            return count
-        return recurse(['dummy'])
-    """
+    
+
