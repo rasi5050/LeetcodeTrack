5e298246dd0137e6f176eeb6897eec66032c18a4
Rasi <rasi5050@gmail.com>
2023-01-10 14:26:58 -0500

    Time: 1430 ms (85.22%), Space: 29.5 MB (20.97%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,32 +1,11 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        #do brute force
-        """TLE
-        res=[]
-        for i in range(len(temperatures)):
-            res.append(0)
-            for j in range(i+1, len(temperatures)):
-                if temperatures[j]>temperatures[i]:
-                    res[i]=j-i
-                    break
-        return res
-        """
-        #use stacks
         res=[0]*len(temperatures)
         stack=[]
-        
-        for i, val in enumerate(temperatures):
-            incoming = (val,i)
-            while stack and stack[-1][0]<incoming[0]:
-                (val,index) = stack.pop()
-                res[index]=i-index
-            stack.append(incoming)
+        for i,temp in enumerate(temperatures):
+            while stack and stack[-1][1]<temp:   #pop if incoming is bigger
+                j,t = stack.pop()
+                res[j]=i-j
+            #after all poping, insert that element
+            stack.append((i, temp))
         return res
-        #status: correct
-        #Analysis: Time O(n); traversing tru all elements once
-        #Space O(n); for storing resultant array
-        #ref: 1/8/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day75/76-c3AiDay1/2,0.preserveStreak,1.dailyTemperaturesTImed25Mins-x1pomo(18:00-18:30),2.implement-x2pomo(18:30-19:30),3.absorber-x1pomo(19:30-20:00)=x4pomo(18:00-20:00)
-
-        
-                    
-                
