0e5e836d76b444d5037d9f2ff5d6ca286e08e525
Rasi <rasi5050@gmail.com>
2023-01-11 13:32:55 -0500

    Time: 22 ms (99.19%), Space: 13.8 MB (70.72%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,28 +1,21 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        stack=[]
-        for char in s:
-            currWord=[]
-            currNum=[]
-            
-            #keep popping
-            if char ==']':
-                while stack[-1]!='[':
-                    currWord.append(stack.pop())
-                stack.pop()         #poping opening bracket
-                curWord="".join(currWord)
-                currWord=currWord[::-1]
-                while stack and stack[-1].isdigit():
-                    currNum.append(stack.pop())
-                currNum=int("".join(currNum)[::-1])
-                stack+=list(currWord*currNum)
-            else:    
-                stack.append(char)
-        return ''.join(stack)
-    
-        #status: correct
-        #Analaysis: Time O(n); traversing tru elements once
-        #Space O(n); storing stack
-        #ref: 1/8/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day75/76-c3AiDay1/2,0.preserveStreak,1.dailyTemperaturesTImed25Mins-x1pomo(18:00-18:30),2.implement-x2pomo(18:30-19:30),3.absorber-x1pomo(19:30-20:00)=x4pomo(18:00-20:00)
-
         
+        stack=[]
+        for c in s:
+            k=[]
+            block=[]
+            if c==']':
+                while stack[-1]!='[':
+                    block.append(stack.pop())
+                stack.pop()             #pop opening bracket
+                while stack and stack[-1].isdigit():
+                    k.append(stack.pop())
+                # print(k, block)
+                k=int("".join(k[::-1]))
+                block="".join(block[::-1])
+                stack.extend(k*block)
+                continue
+            
+            stack.append(c)
+        return "".join(stack)
