d5d72b5fde8283091d3f6c0fadff20a8763fa633
Rasi <rasi5050@gmail.com>
2023-01-13 10:06:03 -0500

    Time: 98 ms (91.58%), Space: 16.8 MB (59.16%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,24 +1,22 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        #create adjacency list
-        adj = { i:[] for i in range(numCourses)}
-        for req in prerequisites:
-            a,b=req
-            adj[a].append(b)
-        # print(adj)    
-        
-        visited=set()
-        def dfs(course):
-            if course in visited: return False
-            if adj[course] == []: return True
-            visited.add(course)
-            for pre in adj[course]:
-                if not dfs(pre): return False
-            visited.remove(course)    
-            adj[course]=[]
-            return True
-                
-                
-        for req in prerequisites:
-            if dfs(req[0])==False: return False
+        map=defaultdict(list)
+        for req, pre in prerequisites:
+            map[req].append(pre)
+        visited=set()   
+        def recurse(req):
+            if req in visited:
+                return False
+            visited.add(req)           
+            for each in map[req]:
+                if recurse(each)==False:
+                    return False
+            del map[req]
+            visited.remove(req)
+        for i in range(numCourses):    
+            if recurse(i)==False:
+                return False
         return True
+        
+        
+        
