a21a750f760f943d0faecd066fa3a3b9f841994b
Rasi <rasi5050@gmail.com>
2023-01-13 10:49:47 -0500

    Time: 100 ms (88.28%), Space: 17.6 MB (71.84%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -18,3 +18,8 @@ class Solution:
         for word in strs:
             map[prod([primes[c] for c in word])].append(word)
         return map.values()
+    
+        #status: correct
+        #Analysis: Time O(n*k), Space O(n*k); where n is the number of words, k is the word length
+        #ref: #ref: 1/13/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day80/80-,0.void-x1pomo(5:30-6:00),1.serialializeAndDeserializeBinaryTreeTimed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.courseScheduleTimed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.groupAnagramsTimed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.findAllAnagramsInAStringTimed25Mins-x1pomo(10:30-11:00),8.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)delayed(6:00-11:30)
+    
