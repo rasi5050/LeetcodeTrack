1a6729547ea05ca5c27fb85e8a57b14c69707ab6
Rasi <rasi5050@gmail.com>
2023-01-14 11:44:46 -0500

    Time: 339 ms (43.35%), Space: 15.6 MB (7.72%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.py
@@ -34,7 +34,7 @@ class Solution:
         
     
         #status: correct (idea from leetcode solution)
-        #Analysis: Time O(n), Space O(1)
+        #Analysis: Time O(nlogn), Space O(1)
         #ref: 1/14/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day81/82C3Ai3QuestionsFromMostFrequentListOf10Day1/5+Blind75WeekOne14q,2q/dayDay1/7-,0.void-x4pomo(5:30-7:30),1.3.JP Morgan Chase & Co. – NAMR SEP - Campus Hiring - 2023 Batch:lastDateUnknown:assumingLastDate1/15/2023;doOn1/14/2023-x2pomo(6:30-7:30),2.addQuestionsToDrive-x1pomo(7:30-8:00),3.searchInRotatedSortedArrayTimed25Mins-x1pomo(8:00-8:30),4.implement-x2pomo(8:30-9:30),5.largestPerimeterTriangleTimed25Mins-x1pomo(9:30-10:00),6.implement-x2pomo(10:00-11:00),7.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)delayed(7:30-11:30)delayed;1.searchInRotatedSortedArrayTimed25Mins-x1pomo(10:30-11:00),2.implement-x1pomo(11:00-11:30)
 
         
