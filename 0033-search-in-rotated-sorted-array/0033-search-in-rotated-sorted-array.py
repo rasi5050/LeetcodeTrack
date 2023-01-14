bc94ecf97240d21a54246545f97a546ae48645e9
Rasi <rasi5050@gmail.com>
2023-01-14 10:49:40 -0500

    Time: 47 ms (67.21%), Space: 14.1 MB (89.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,44 +1,24 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
-        #intuition: find pivot, apply binary search on left side and right side of pivot separately
-        
-        #steps:
-        #identify pivot
-        #1.for i in range(len(nums)-1):
-        #2.if nums[i]>nums[i+1]: pivot=i+1;break
-        #3.appy merge sort on left and right
-        #4.return index, else return -1
-        
-        if len(nums)==1 and nums[0]==target:
-            return 0
-        
-        pivot=len(nums)-1
+        #find pivot and do binary search on left and right segments
+        pivot=0
         for i in range(len(nums)-1):
             if nums[i]>nums[i+1]:
                 pivot=i+1
-                break
-        #merge sort
-        left=0
-        right=pivot-1
-        if nums[left]<=target<=nums[right]:
-            while(left<=right):
+                
+        def binarySearch(left, right):
+            while left<=right:
                 mid=(left+right)//2
                 if nums[mid]==target:
                     return mid
-                elif target<nums[mid]:
+                elif nums[mid]>target:
                     right=mid-1
                 else:
                     left=mid+1
-        else:
-            left=pivot
-            right=len(nums)-1
-            while(left<=right):
-                mid=(left+right)//2
-                if nums[mid]==target:
-                    return mid
-                elif target<nums[mid]:
-                    right=mid-1
-                else:
-                    left=mid+1
+        inLeft=binarySearch(0, pivot-1)
+        inRight=binarySearch(pivot, len(nums)-1)
+        if inLeft or inLeft==0: return inLeft
+        if inRight or inRight==0: return inRight
         return -1
+                
+        
