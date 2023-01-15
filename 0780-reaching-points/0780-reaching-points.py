393ebde986db1d9d6551e8e9e3f53a03ff942846
Rasi <rasi5050@gmail.com>
2023-01-15 10:17:37 -0500

    Time: 39 ms (57.12%), Space: 13.8 MB (82.05%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.py
+++ 0780-reaching-points/0780-reaching-points.py
@@ -35,3 +35,7 @@ class Solution:
             one straight branch to the right (sx+k*sy, sy)  ie. sy==ty and (tx-sx)%sy==0
         """
         return sy==ty and tx>=sx and (tx-sx)%sy==0 or sx==tx and ty>=sy and (ty-sy)%sx==0
+    
+    #status: success; idea from (https://leetcode.com/problems/reaching-points/discuss/114856/JavaC%2B%2BPython-Modulo-from-the-End)
+    #Analysis: Time O(log(min(tx,ty))), Space O(1)
+    #ref: 1/15/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day82/82C3Ai3QuestionsFromMostFrequentListOf0/10Day2/5+Blind75WeekOne0/14q,2q/dayDay2/7-,1.countVowelsPermutationTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.reachingPointsTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.longestArithmeticSubsequenceTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),Blind753q/Day,7.2SumTimed25Mins-x1pomo(10:00-10:30),8.validParenthesesTImed25Mins-x1pomo(10:30-11:00),9.mergeTwoSortedListsTimed25Mins-x1pomo(11:00-11:30=x12pomo(5:30-11:30)  
