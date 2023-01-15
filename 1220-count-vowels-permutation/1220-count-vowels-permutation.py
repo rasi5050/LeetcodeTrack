9de4589050044559cde5f90169d59bc5850030f5
Rasi <rasi5050@gmail.com>
2023-01-15 07:14:58 -0500

    Time: 774 ms (37.95%), Space: 55 MB (33.56%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,51 +1,19 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        """
-        map={'a':['e'],
-            'e': ['a','i'],
-            'i': ['a','e','o','u'],
-            'o': ['i','u'],
-            'u': ['a']}
-        
-        a -> e
-        e -> a|i
-        i -> a|e|o|u
-        o -> i|u
-        u -> a
-        """
-        #TLE
-        """
-        counter=[0]
-        def recurse(s,char):
-            # if s in visited:
-            #     return
-            if len(s)==n:
-                counter[0]+=1
-                return     
-            for edge in map[char]:
-                recurse(s+edge, edge)
-        for char in map:
-            recurse(char, char)
-        return counter[0]
-        """
         map={
-            "": ['a','e','i','o','u'],
+            '.': ['a','e','i','o','u'],
             'a': ['e'],
             'e': ['a','i'],
             'i': ['a','e','o','u'],
-            'o': ['i', 'u'],
-            'u': ['a']
+            'o': ['i','u'],
+            'u': ['a'],
         }
-        
-        memo={}
-        def dfs(i, char):
-            if (i, char) in memo: return memo[(i, char)]
-            if i==0:
+        @lru_cache(None)
+        def recurse(s,cur):
+            if s==n:
                 return 1
-            
-            memo[(i, char)]=0
-            
-            for edge in map[char]:
-                memo[(i, char)] = (memo[(i, char)] + dfs(i-1, edge)) % 1000000007
-            return memo[(i, char)]
-        return dfs(n, "") % 1000000007
+            count=0
+            for mapping in map[cur]:
+                count = (count+recurse(s+1, mapping))%1000000007
+            return count
+        return recurse(0,'.')
