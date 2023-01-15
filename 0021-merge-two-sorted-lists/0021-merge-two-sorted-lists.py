ea6039378f217a149ecbcf0613d18f22f1df2dcd
Rasi <rasi5050@gmail.com>
2023-01-15 15:27:47 -0500

    Time: 37 ms (86.00%), Space: 13.9 MB (75.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -10,14 +10,13 @@ class Solution:
         sentinel=trail=ListNode()
         while list1 and list2:
             if list1.val<list2.val:
-                trail.next=ListNode(list1.val)
+                trail.next=list1
                 trail,list1=trail.next,list1.next
             else:
-                trail.next=ListNode(list2.val)
+                trail.next=list2
                 trail,list2=trail.next,list2.next
         if list1:
             trail.next=list1
         if list2:
-            trail.next=list2
-                
+            trail.next=list2     
         return sentinel.next
