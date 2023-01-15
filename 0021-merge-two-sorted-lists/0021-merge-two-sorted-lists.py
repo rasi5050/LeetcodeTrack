c5c556600a9fc9ee9c825e09b303c4bdee91e781
Rasi <rasi5050@gmail.com>
2023-01-15 15:26:43 -0500

    Time: 46 ms (53.21%), Space: 14 MB (29.15%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,24 +5,19 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head=temp=ListNode()
-        while list1 and list2:
-            if list1.val<=list2.val:
-                temp.next=list1
-                list1=list1.next
-            else:
-                temp.next=list2
-                list2=list2.next
-            temp=temp.next
-        if list1:
-            temp.next=list1
-        elif list2:
-            temp.next=list2
-        return head.next
-    
-    #status: correct
-    #ref:11/12/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day37/38:do,1.mergeTwoSortedListTimed25Mins-x1pomo(10:00-10:30),8.implement-x2pomo(10:30-11:30)=x3pomo(18:00-19:30)
-    
-            
-                
         
+        
+        sentinel=trail=ListNode()
+        while list1 and list2:
+            if list1.val<list2.val:
+                trail.next=ListNode(list1.val)
+                trail,list1=trail.next,list1.next
+            else:
+                trail.next=ListNode(list2.val)
+                trail,list2=trail.next,list2.next
+        if list1:
+            trail.next=list1
+        if list2:
+            trail.next=list2
+                
+        return sentinel.next
