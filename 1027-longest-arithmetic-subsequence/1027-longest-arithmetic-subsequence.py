5bc8902571deb01983b1b73f32a6d5729c73ae28
Rasi <rasi5050@gmail.com>
2023-01-15 14:02:46 -0500

    Time: 3370 ms (73.16%), Space: 66.5 MB (14.89%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.py
@@ -1,11 +1,10 @@
 class Solution:
     def longestArithSeqLength(self, nums: List[int]) -> int:
-        dp={}
+        map={}
         for i in range(len(nums)):
+            flag=True
             for j in range(i+1, len(nums)):
-                dp[(j, nums[j]-nums[i])] = dp.get((i,nums[j]-nums[i]), 1) + 1
-        return max(dp.values())
+                diff=nums[j]-nums[i]
+                map[j, diff]=map.get((i,diff),1)+1
+        return max(map.values())
                 
-    #status: correct; didnot understand; idea from leetcode discussions solutions (https://leetcode.com/problems/longest-arithmetic-subsequence/discuss/274611/JavaC%2B%2BPython-DP)
-    #Analysis: Time O(n^2), Space O(n^2)
-    #ref:1/9/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day76/76-,1.countVowelsPermutationsTimed25Mins-x1pomo(18:00-18:30),2.implement-x2pomo(18:30-19:30),3.absorber-x1pomo(19:30-20:00)=x4pomo(18:00-20:00)early(12:30-14:30)              
