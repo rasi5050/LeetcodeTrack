b439edd19a19bdf4c932d268a4e2caceebf83491
Rasi <rasi5050@gmail.com>
2023-01-16 15:59:37 -0500

    Time: 33 ms (60.79%), Space: 13.9 MB (98.47%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -51,6 +51,6 @@ class MyQueue:
         return (not self.front and not self.back)
 
     #status: correct
-    # Analaysis: Time O(n^2); say, if front has n elements, and if we want to pop( queue, then all elements of front has to be flipped to back), Space O(n)
+    # Analaysis: Time O(n); say, if front has n elements, and if we want to pop( queue, then all elements of front has to be flipped to back), Space O(n)
     #ref: 1/16/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day83/84C3Ai3QuestionsFromMostFrequentListOf3/10Day3/5+Blind75WeekOne7/14q,2q/dayDay3/7-,1.balancedBinaryTreeTImed25Mins-x1pomo(12:30-13:00),2.linkedListCycleTimed25Mins-x1pomo(13:00-13:30),3.implement-x1pomo(13:30-14:00),4.implementQueueUsingStacksTimed25Mins-x1pomo(14:00-14:30),5.firstBadVersionTimed25Mins-x1pomo(14:30-15:00),6.implement-x1pomo(15:00-15:30),7.ransomNoteTImed25Mins-x1pomo(15:30-16:00),8.implement-x1pomo(16:00-16:30)=x8pomo(12:30-16:30)                   
 
