b4c267e8260110d289547b6a943fad7a5fd60767
Rasi <rasi5050@gmail.com>
2023-01-16 16:56:48 -0500

    Time: 51 ms (85.11%), Space: 14.2 MB (51.25%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,13 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        """working code; commented for alter
         magazineCounter=Counter(magazine)
         ransomCounter=Counter(ransomNote)
         return prod([False for char in ransomCounter if ransomCounter[char]>magazineCounter[char]])
+        """
+        r=Counter(ransomNote)
+        m=Counter(magazine)
+        for char in r:
+            if m[char]<r[char]:         #magazine doesnt have enough char that of in ransomNote
+                return False
+        return True
