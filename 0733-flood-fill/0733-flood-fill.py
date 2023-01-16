a7fa3dec9551f5658823acacbdec5270cfd05928
Rasi <rasi5050@gmail.com>
2023-01-15 20:39:22 -0500

    Time: 68 ms (98.89%), Space: 14.2 MB (36.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -2,14 +2,11 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
         sourceColor=image[sr][sc]
         if sourceColor==color: return image
-        visited=set()
         def dfs(x,y):
             #base conditions
             if x<0 or y<0 or x>len(image)-1 or y>len(image[0])-1 or image[x][y]!=sourceColor:
                 return
-            
             image[x][y]=color
-            # visited.add((x,y))
             dfs(x+1,y)
             dfs(x-1,y)
             dfs(x,y+1)
