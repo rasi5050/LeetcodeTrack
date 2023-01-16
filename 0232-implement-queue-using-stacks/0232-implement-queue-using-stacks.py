652f59a38c9d74727b3da1d345245074aa89a8b8
Rasi <rasi5050@gmail.com>
2023-01-16 15:11:25 -0500

    Time: 30 ms (78.28%), Space: 13.9 MB (65.94%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,37 +1,21 @@
-from collections import deque
 class MyQueue:
 
     def __init__(self):
-        self.q1=[]
-        self.qTemp=[]
+        self.q=deque()
 
     def push(self, x: int) -> None:
-        self.q1.append(x)
+        return self.q.append(x)
 
     def pop(self) -> int:
-        while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
-        while self.qTemp:
-            self.q1.append(self.qTemp.pop())
-        return tempVar
+        return self.q.popleft()
 
     def peek(self) -> int:
-        while self.q1:
-            self.qTemp.append(self.q1.pop())
-        tempVar=self.qTemp.pop()
-        self.q1.append(tempVar)
-        while self.qTemp:
-            self.q1.append(self.qTemp.pop())
-        return tempVar
+        return self.q[0]
 
     def empty(self) -> bool:
-        return True if len(self.q1)==0 else False
-    #analysis time O(nk)=O(n), n: no of elements, k=number of operations, since peek and pop take k=2 each.
-    #space: O(2n)=(n)
-    
-    
-    
+        return False if self.q else True
+
+
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
