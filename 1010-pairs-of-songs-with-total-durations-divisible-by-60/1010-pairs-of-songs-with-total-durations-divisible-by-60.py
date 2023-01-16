17de2529a7149660bbc1771511dbe5cccf68f665
Rasi <rasi5050@gmail.com>
2023-01-16 06:41:32 -0500

    Time: 347 ms (54.23%), Space: 17.3 MB (99.50%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,17 +1,14 @@
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        #convert problem into 2 sum by modding all value by 60
+        #convert to %60
         for i in range(len(time)):
             time[i]%=60
-        
-        map={}
-        total=0
-        for t in time:
-            # if t==0 and t in map:
-            #     total+=map[t]
-            if (60-t)%60 in map:
-                total+=map[(60-t)%60]
-            map[t] = map.get(t,0) + 1
-        return total
-                
-        
+        print(time)
+        #now its 2Sum problem for target=60
+        count=0
+        map=defaultdict(int)
+        for j,val in enumerate(time):
+            if (60-val)%60 in map:
+                count+=map[(60-val)%60]
+            map[val]+=1
+        return count
