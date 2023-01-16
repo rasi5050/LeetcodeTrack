b7bf475a46512bb6507837a12a721cb6d3419529
Rasi <rasi5050@gmail.com>
2023-01-16 15:27:17 -0500

    Time: 41 ms (22.79%), Space: 14 MB (15.86%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,5 +1,5 @@
 class MyQueue:
-
+    """working code; commented for alter
     def __init__(self):
         self.q=deque()
 
@@ -15,7 +15,7 @@ class MyQueue:
     def empty(self) -> bool:
         return False if self.q else True
 
-
+    """
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
 # obj.push(x)
@@ -26,4 +26,27 @@ class MyQueue:
     #status: correct
     #Analysis: Time O(n) Space O(n)
     #ref: 1/16/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day83/84C3Ai3QuestionsFromMostFrequentListOf3/10Day3/5+Blind75WeekOne7/14q,2q/dayDay3/7-,1.balancedBinaryTreeTImed25Mins-x1pomo(12:30-13:00),2.linkedListCycleTimed25Mins-x1pomo(13:00-13:30),3.implement-x1pomo(13:30-14:00),4.implementQueueUsingStacksTimed25Mins-x1pomo(14:00-14:30),5.firstBadVersionTimed25Mins-x1pomo(14:30-15:00),6.implement-x1pomo(15:00-15:30),7.ransomNoteTImed25Mins-x1pomo(15:30-16:00),8.implement-x1pomo(16:00-16:30)=x8pomo(12:30-16:30)                   
+
+    #alter; implement using 2 stacks
+    def __init__(self):
+        self.front=[]
+        self.back=[]
+
+    def push(self, x: int) -> None:
+        while self.back:
+            self.front.append(self.back.pop())
+        self.front.append(x)
+
+    def pop(self) -> int:
+        while self.front:
+            self.back.append(self.front.pop())
+        return self.back.pop()
+
+    def peek(self) -> int:
+        if self.front:
+            return self.front[0]
+        return self.back[-1]
+
+    def empty(self) -> bool:
+        return False if (self.front or self.back) else True
 
