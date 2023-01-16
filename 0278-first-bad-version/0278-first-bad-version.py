25514876c33331faddee38bde5863dc4c5ce1ff8
Rasi <rasi5050@gmail.com>
2023-01-16 16:19:24 -0500

    Time: 35 ms (62.44%), Space: 13.8 MB (58.12%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -4,8 +4,8 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         
-        
-        #something to do with binary search
+
+        #intuition: something to do with binary search
         left=1
         right=n
         while left<=right:
@@ -18,3 +18,6 @@ class Solution:
                 left=mid+1
             else:
                 right=mid-1
+        #status: correct
+        #Analysis: Time O(log n), Space O(1)
+        #ref: 1/16/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day83/84C3Ai3QuestionsFromMostFrequentListOf3/10Day3/5+Blind75WeekOne7/14q,2q/dayDay3/7-,1.balancedBinaryTreeTImed25Mins-x1pomo(12:30-13:00),2.linkedListCycleTimed25Mins-x1pomo(13:00-13:30),3.implement-x1pomo(13:30-14:00),4.implementQueueUsingStacksTimed25Mins-x1pomo(14:00-14:30),5.firstBadVersionTimed25Mins-x1pomo(14:30-15:00),6.implement-x1pomo(15:00-15:30),7.ransomNoteTImed25Mins-x1pomo(15:30-16:00),8.implement-x1pomo(16:00-16:30)=x8pomo(12:30-16:30)                   
