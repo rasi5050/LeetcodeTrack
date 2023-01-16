21af727c258d57a194c162820eda105d5495a985
Rasi <rasi5050@gmail.com>
2023-01-16 14:25:08 -0500

    Time: 71 ms (49.29%), Space: 17.8 MB (18.91%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -14,3 +14,18 @@ class Solution:
             visited.add(id(head))
             head=head.next
         return False
+    
+    #status: correct
+    #Analysis: Time O(n), Space O(n)
+    #ref: 1/16/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day83/84C3Ai3QuestionsFromMostFrequentListOf3/10Day3/5+Blind75WeekOne7/14q,2q/dayDay3/7-,1.balancedBinaryTreeTImed25Mins-x1pomo(12:30-13:00),2.linkedListCycleTimed25Mins-x1pomo(13:00-13:30),3.implement-x1pomo(13:30-14:00),4.implementQueueUsingStacksTimed25Mins-x1pomo(14:00-14:30),5.firstBadVersionTimed25Mins-x1pomo(14:30-15:00),6.implement-x1pomo(15:00-15:30),7.ransomNoteTImed25Mins-x1pomo(15:30-16:00),8.implement-x1pomo(16:00-16:30)=x8pomo(12:30-16:30)  
+    
+        #alter; floyd cycle detection algorithm
+        slow=head
+        fast=head.next
+        
+        while fast:
+            if slow==fast:
+                return True
+            slow=slow.next
+            fast=fast.next.next
+        return False
