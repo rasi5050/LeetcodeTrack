aa8c8b37e591dfe653444b4c9875ceec6e44dabe
Rasi <rasi5050@gmail.com>
2023-01-17 11:00:05 -0500

    Time: 30 ms (97.23%), Space: 15.4 MB (92.66%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,36 +5,22 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         head2=ListNode() #pointer to result
-#         tempHead=head2
-#         res=[]
-#         def reverseLl(head):
-#             nonlocal tempHead
-#             if head.next:
-#                 reverseLl(head.next)
-#             tempHead.next=ListNode(head.val)
-            
-#             print(tempHead)
-#         reverseLl(head)
-#         return head2
-
-        #approach2: reverse direction(from leetcode discussion)
-        # prev=None
-        # cur=head
-        # while cur:
-        #     next=cur.next
-        #     cur.next=prev
-        #     prev=cur
-        #     cur=next
-        # return prev
-        #approach3: reverse direction iterative changed to recursive(neetcode youtube comments:https://www.youtube.com/watch?v=G0_I-ZF0S38)
-        
-        def reverseLl(prev,head):
-            if not head: return prev
+        """        
+        while head.next:
+            temp=head.next
+            curr=head
             next=head.next
-            head.next=prev
-            return reverseLl(head,next)
-        return reverseLl(None,head)
-    
-        #status: correct
+            next.next=curr
+            head=temp
+            
+        return head
+        """
+        prev, curr = None, head
+        
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
         
