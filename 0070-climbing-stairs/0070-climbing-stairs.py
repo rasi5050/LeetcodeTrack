75abe4ae67566ef48abfb715a727e4730e869060
Rasi <rasi5050@gmail.com>
2023-01-17 09:19:42 -0500

    Time: 28 ms (91.82%), Space: 13.7 MB (96.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,6 +3,5 @@ class Solution:
         dp=[0]*(n+2)
         dp[n]=1
         for i in range(n-1, -1, -1):
-            dp[i]=dp[i+1]+dp[i+2]
-        print(dp)    
+            dp[i]=dp[i+1]+dp[i+2]   
         return dp[0]
