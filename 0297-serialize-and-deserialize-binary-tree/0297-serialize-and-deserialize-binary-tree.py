c232ffb7f3d19513f1d42da0c2b5a929dd077ac5
Rasi <rasi5050@gmail.com>
2023-01-17 08:45:04 -0500

    Time: 120 ms (81.77%), Space: 20.4 MB (43.54%) - LeetHub

--- 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.py
@@ -13,12 +13,10 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        if not root:
+            return '_;'
+        return '' + str(root.val) + ';' + str(self.serialize(root.left)) + str(self.serialize(root.right))
         
-        def recurse(root):
-            if not root: 
-                return '_;'   
-            return '' + str(root.val) + ';' + str(recurse(root.left)) + str(recurse(root.right))
-        return recurse(root)
         
 
     def deserialize(self, data):
@@ -27,33 +25,28 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        
         i=0
         root=TreeNode()
-        def recurse(root):
-            nonlocal i 
+        def decode(root):
+            nonlocal i
             if data[i]=='_':
                 i+=2
                 return None
             else:
                 num=''
-                while (data[i].isdigit()) or data[i]=='-':
+                while data[i].isdigit() or data[i]=='-':        #accounting for -ve numbers
                     num+=data[i]
                     i+=1
                 i+=1
                 root=TreeNode(num)
-                root.left=recurse(root.left)
-                root.right=recurse(root.right)
+                root.left=decode(root.left)
+                root.right=decode(root.right)
             return root
-        return recurse(root)            
+        return decode(root)
+                    
         
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
 # deser = Codec()
 # ans = deser.deserialize(ser.serialize(root))
-
-        #status: correct
-        #Analysis: Time O(n); traversing tru each element once
-        #Space O(n); storing results
-        #ref: 1/13/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day80/80-,0.void-x1pomo(5:30-6:00),1.serialializeAndDeserializeBinaryTreeTimed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.courseScheduleTimed25Mins-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.groupAnagramsTimed25Mins-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.findAllAnagramsInAStringTimed25Mins-x1pomo(10:30-11:00),8.absorber-x1pomo(11:00-11:30)=x12pomo(5:30-11:30)delayed(6:00-11:30)
