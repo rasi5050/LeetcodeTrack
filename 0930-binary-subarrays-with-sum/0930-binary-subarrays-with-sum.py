370892b6bf4463a01f81a7c85eeef899f549c0a6
Rasi <rasi5050@gmail.com>
2023-01-17 06:56:45 -0500

    Time: 351 ms (49.17%), Space: 19.5 MB (5.50%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,11 +1,31 @@
-from collections import Counter
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        counter, prefSum, count = Counter({0:1}), 0, 0
-        for num in nums:
-            prefSum+=num
-            count+=counter[prefSum-goal]
-            counter[prefSum]+=1
+        """working code; TLE
+        sumList=sum(nums)
+        if sumList<goal: return 0
+        if sumList==goal: return 1
+        count=0
+        for i in range(len(nums)):
+            curSum=nums[i]
+            if curSum==goal:
+                count+=1
+            for j in range(i+1, len(nums)):
+                curSum+=nums[j]
+                if curSum==goal:
+                    count+=1
+                elif curSum>goal: break
         return count
-   
+        """        
+        #something to do with prefix sums
         
+        
+        P=[0]
+        for x in nums: P.append(P[-1]+x)
+        print(P)
+        
+        count=Counter()
+        ans=0
+        for x in P:   #for each value in prefix sum
+            ans+=count[x]
+            count[x+goal] += 1
+        return ans
