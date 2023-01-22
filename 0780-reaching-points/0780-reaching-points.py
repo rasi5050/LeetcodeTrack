5b3e2645b6d346d23ac56dc5240157cf3fb0d4ce
Rasi <rasi5050@gmail.com>
2023-01-22 13:43:40 -0500

    Time: 51 ms (35.51%), Space: 13.8 MB (79.28%) - LeetHub

--- 0780-reaching-points/0780-reaching-points.py
+++ 0780-reaching-points/0780-reaching-points.py
@@ -36,3 +36,9 @@ class Solution:
         if sx==tx and ty>=sy and (ty-sy)%sx==0: return True
         if sy==ty and tx>=sx and (tx-sx)%sy==0: return True
         return False
+    
+        #status: correct
+        #Analysis: Time(log(max(tx,ty))), Space O(1)
+        #ref: 1/22/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day89/90Blind26/75,2q/dayDay8/10;0.absorber-x1pomo(6:00-6:30),1.predictiveIndexBehavioralAssesment;lastDate2/16/2023-x1pomo(6:30-7:00),3.;P1:doTiktok:SWE OA(DevOps)- 2023 Summer (BS/MS)Day1/5-3q/day+searchOAQuestionsInReddit/csMajor,1.numberOfWaysOfCuttingPizzaTimed25Mins-x1pomo(7:00-7:30),2.implement-x2pomo(7:30-8:00),3.reachingPointsTimed25Mins-x1pomo(8:00-8:30),4.minimumIncrementToMakeArrayUniqueTimed25Mins-x1pomo(8:30-9:00),5.implement-x2pomo(9:00-10:00),6.01MatrixTimed25Mins-x1pomo(10:00-10:30),7.implement-x1pomo(10:30-11:00)=x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)=delayed=>1.predictiveIndexBehavioralAssesment;lastDate2/16/2023-x1pomo(7:30-8:00),3.;P1:doTiktok:SWE OA(DevOps)- 2023 Summer (BS/MS)Day1/5-3q/day+searchOAQuestionsInReddit/csMajor,1.numberOfWaysOfCuttingPizzaTimed25Mins-x1pomo(8:00-8:30),2.implement-x2pomo(8:30-9:30),3.reachingPointsTimed25Mins-x1pomo(9:30-10:00),4.minimumIncrementToMakeArrayUniqueTimed25Mins-x1pomo(10:00-10:30),5.implement-x2pomo(10:30-11:00)=alter=>1.predictiveIndexBehavioralAssesment;lastDate2/16/2023-x1pomo(8:30-9:00),3.;P1:doTiktok:SWE OA(DevOps)- 2023 Summer (BS/MS)Day1/5-3q/day+searchOAQuestionsInReddit/csMajor,1.numberOfWaysOfCuttingPizzaTimed25Mins-x1pomo(9:00-9:30),2.implement-x2pomo(9:30-10:30),3.reachingPointsTimed25Mins-x1pomo(10:30-11:00)
+
+        
