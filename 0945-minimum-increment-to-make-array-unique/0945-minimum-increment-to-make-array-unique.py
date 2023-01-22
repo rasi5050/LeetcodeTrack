4685d1e77656fc06755eca67041083c3bdd5de29
Rasi <rasi5050@gmail.com>
2023-01-22 14:21:10 -0500

    Time: 2612 ms (10.95%), Space: 28.3 MB (54.37%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -1,11 +1,9 @@
 class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
         nums.sort()
-        visited=set()
         count=0
         for i in range(1,len(nums)):
             if nums[i-1]>=nums[i]:
                 count+=((nums[i-1]-nums[i])+1)
                 nums[i]+=((nums[i-1]-nums[i])+1)
-            # visited.add(nums[i])
         return count
