a60d01143595b7730321d93a21dd3f9e517b1fcc
Rasi <rasi5050@gmail.com>
2023-01-23 13:18:19 -0500

    Time: 123 ms (42.61%), Space: 14.1 MB (47.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -10,6 +10,6 @@ class Solution:
             lastIndex[c]=r
         return maxLen
     
-    #status: correct
+    #status: correct; help(https://leetcode.com/problems/longest-substring-without-repeating-characters/solution/); approach 3:sliding window optimized
     #Analysis: Time O(n), Space O(n)
     #ref: 1/23/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day90/90Blind26/75,2q/dayDay9/10;1.3SumTImed25Mins-x1pomo(12:00-12:30),2.BinaryTreeLevelOrderTraversalTimed25Mins-x1pomo(12:30-13:00),3.closeGraphTimed25Mins-x1pomo(13:00-13:30),4.implement-x1pomo(13:30-14:00)-x4pomo(12:00-14:00)
