e589df2c6f6ff4dc9e6234019b202c5654688f77
Rasi <rasi5050@gmail.com>
2023-01-23 10:00:29 -0500

    Time: 1422 ms (29.95%), Space: 21 MB (5.96%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,21 +1,28 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        matrix=mat
-        m, n = len(matrix), len(matrix[0])
-        for i, row in enumerate(matrix):
-            for j, cell in enumerate(row):
-                if cell:
-                    top = matrix[i-1][j] if i else float('inf')
-                    left = matrix[i][j-1] if j else float('inf')
-                    matrix[i][j] = min(top, left) + 1        
-        for i in reversed(range(m)):
-            for j in reversed(range(n)):
-                if cell := matrix[i][j]:
-                        bottom = matrix[i+1][j] if i < m - 1 else float('inf')
-                        right = matrix[i][j+1] if j < n -1 else float('inf')
-                        matrix[i][j] = min(cell, min(bottom, right) + 1)
-        return matrix
-    
-    #status: correct (help from leetcode solutions comments of (lenchen1112) https://leetcode.com/problems/01-matrix/solution/)
-    #Analysis: Time O(nm), Space O(1)
-    #ref: 12/22/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day58/58,1.01MatrixTimed25Mins-x1pomo(5:30-6:00),2.implement-x2pomo(6:00-7:00),3.rottingOrangesTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.cloneGraphTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.pacificAtlanticWaterFlowTimed25Mins-x1pomo(10:00-10:30),8.implement-x2pomo(10:30-11:30)=x12pomo(5:30-11:30)
+        
+        #do from all zeroes to as single dummy node to ones
+        frontier=[]
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                if mat[i][j]==0:
+                    frontier.append((i,j))
+        res=[[0]*len(mat[0]) for _ in range(len(mat))]
+        level=0
+        visited=set()
+        while frontier:
+            next=[]
+            for x,y in frontier:
+                if not(x<0 or y<0 or x>len(mat)-1 or y>len(mat[0])-1 or (x,y) in visited):
+                    if mat[x][y]==1:
+                        res[x][y]=level
+                        # continue
+                    visited.add((x,y))
+                    next.append((x-1,y))
+                    next.append((x+1,y))
+                    next.append((x,y-1))
+                    next.append((x,y+1))
+            
+            frontier=next
+            level+=1
+        return res
