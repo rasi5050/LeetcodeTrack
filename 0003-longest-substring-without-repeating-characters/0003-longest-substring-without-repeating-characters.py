b9470436ad988b6dae07d356d88bde3413c6017a
Rasi <rasi5050@gmail.com>
2023-01-23 13:07:49 -0500

    Time: 101 ms (48.90%), Space: 14.2 MB (5.79%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -5,7 +5,7 @@ class Solution:
         maxLen=0
         for r,c in enumerate(s):
             if c in lastIndex:
-                l=max(lastIndex[c],l)
+                l=max(lastIndex[c]+1,l)
             maxLen=max(maxLen, r-l+1)
-            lastIndex[c]=r+1
+            lastIndex[c]=r
         return maxLen
