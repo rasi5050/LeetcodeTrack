50e605167484d3684cbfc2d741efd05581fb1a57
Rasi <rasi5050@gmail.com>
2023-01-24 06:36:14 -0500

    Time: 622 ms (48.03%), Space: 21 MB (62.88%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -1,7 +1,7 @@
 class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
         #do n^2 algorithm
-        
+        """working code; commented to do better
         b=[0]*len(flips)
         preCount=0
         max1Index=0
@@ -9,11 +9,18 @@ class Solution:
         for i in flips:
             b[i-1]=1
             max1Index=max(max1Index, i)
-            #check Prefix
             curLeftSum+=1
             if curLeftSum==max1Index:
                 preCount+=1
         return preCount
-            
+        """
         
-# [1,1,1,1]
+        preCount=0
+        max1Index=0
+        curLeftSum=0
+        for i in flips:
+            max1Index=max(max1Index, i)
+            curLeftSum+=1
+            if curLeftSum==max1Index:
+                preCount+=1
+        return preCount
