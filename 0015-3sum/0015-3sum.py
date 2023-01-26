90c9b3c237fc5a42354c4de2cee1e71ace318689
Rasi <rasi5050@gmail.com>
2023-01-26 11:46:33 -0500

    Time: 1702 ms (49.13%), Space: 18.4 MB (22.85%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -27,4 +27,6 @@ class Solution:
             twoSum2(index, target)
         return res
         
-        
+        #status: correct
+        #Analysis: Time O(n^2), Space O(n)
+        #ref: 1/26/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day93/94Blind29/75,2q/dayDay12/12;1.P1:doOA:1.robotRoomCleaner-x1pomo(6:00-6:30),2.implement-x1pomo(6:30-7:00),3.numberOfGoodLeafNodesPairsTimed25Mins-x1pomo(7:00-7:30),4.implement-x1pomo(7:30-8:00),5.3SumTimed25Mins-x1pomo(8:00-8:30),6.implement-x1pomo(8:30-9:00),7.binaryTreeLevelOrderTraversalTimed25Mins-x1pomo(9:00-9:30),8.cloneGraph-x1pomo(9:30-10:00),9.Evaluate Reverse Polish NotationTimed25Mins-x1pomo(10:00-10:30),10.implement-x1pomo(10:30-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
