32f25eb2837aeecc58dfe8d2c610ae9d437b019e
Rasi <rasi5050@gmail.com>
2023-01-27 10:07:06 -0500

    Time: 52 ms (32.74%), Space: 14.2 MB (78.92%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -6,20 +6,18 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        #bfs solution
+        #do BFS
         res=[]
-        if not root: return []
         frontier=[root]
         while frontier:
             next=[]
             temp=[]
             for node in frontier:
-                temp.append(node.val)
-                if node.left: next.append(node.left)
-                if node.right: next.append(node.right)
-            frontier=next[:]
+                if node:
+                    temp.append(node.val)
+                    next.append(node.left)
+                    next.append(node.right)    
+            frontier=next
             res.append(temp)
-        return res
-    #status: correct
-    #analysis: Time O(n), Space O(n)
-    #ref: 12/18/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day54/54,1.binaryTreeMaximumPathSumTimed25Mins-x1pomo(7:00-7:30),2.implement-x2pomo(7:30-8:30)=x3pomo(7:00-8:30)
+        return res[:-1]
+        
