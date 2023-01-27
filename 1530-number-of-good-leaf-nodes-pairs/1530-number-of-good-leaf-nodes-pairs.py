b99a41cee2a4d9400c386c17fda55e7290593afb
Rasi <rasi5050@gmail.com>
2023-01-27 07:56:10 -0500

    Time: 132 ms (81.30%), Space: 15.4 MB (28.31%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -29,3 +29,8 @@ class Solution:
             return [1+d for d in leftDistances+rightDistances]
         recurse(root)
         return count
+    
+    #status: correct; help(https://www.youtube.com/watch?v=3JU0v2kuYGg&t=552s)
+    #Analysis: Time O(N^2); nested for loop inside recursion
+    #Space O(N)
+    #ref: 1/27/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day94/94Blind30/75,2q/dayDay13/12;doLeetcode3Question/Day:1.P1:1.numberOfGoodLeafNodesPairsTimed25Mins-x1pomo(6:00-6:30),2.implement-x1pomo(6:30-7:00),3.productOnLastKNumbersTimed25Mins-x1pomo(7:00-7:30),4.implement-x1pomo(7:30-8:00),5.binaryTreeLevelOrderTraversalTimed25Mins-x1pomo(8:00-8:30),6.cloneGraph-x1pomo(8:30-9:00),7.Evaluate Reverse Polish NotationTimed25Mins-x1pomo(9:00-9:30),8.implement-x1pomo(9:30-10:00),9.courseSchedule-x1pomo(10:00-10:30)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)early(6:00-10:30)
