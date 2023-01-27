e55858077d29defdc0b91801021a2e71eb69d548
Rasi <rasi5050@gmail.com>
2023-01-27 08:54:26 -0500

    Time: 3492 ms (5.36%), Space: 29.4 MB (28.82%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.py
@@ -2,12 +2,28 @@ class ProductOfNumbers:
     #do naive algorithm
     def __init__(self):
         self.stream=[]
-
+        self.prefixProd=[]
+    #do as prefix prod
     def add(self, num: int) -> None:
+        if self.stream:
+            self.prefixProd.append(self.prefixProd[-1]*num)
+        else:
+            self.prefixProd.append(num)
         self.stream.append(num)
 
     def getProduct(self, k: int) -> int:
-        return(prod(self.stream[len(self.stream)-k:]))
+        # print(self.stream, self.prefixProd, k)
+        if k==len(self.stream):
+            return self.prefixProd[-1]
+        elif self.prefixProd[-1]==0 and self.prefixProd[-(k+1)]!=0: #zero is within the last k
+                return 0
+            
+        elif self.prefixProd[-1]==0 and self.prefixProd[-(k+1)]==0: #zero can from somewhere else; hence need to calculate actual product
+            return(prod(self.stream[len(self.stream)-k:]))
+        return self.prefixProd[-1]//self.prefixProd[-(k+1)]   #if values other than zeroes
+            
+            
+        
 
 
 # Your ProductOfNumbers object will be instantiated and called as such:
