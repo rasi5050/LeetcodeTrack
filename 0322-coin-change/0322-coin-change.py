84ab9836aad004c9a4d3637d44500fce4cb3bfa0
Rasi <rasi5050@gmail.com>
2023-01-30 10:18:37 -0500

    Time: 1506 ms (72.39%), Space: 14.2 MB (59.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -10,6 +10,6 @@ class Solution:
         # print(dp)
         return dp[-1] if dp[-1]!=math.inf else -1
     
-    #status: correct; did not understand
+    #status: correct; did not understand; help(https://www.youtube.com/watch?v=H9bfqozjoqs&t=953s)
     #Analysis: Time O(n^2), space (n)
     #ref: 1/29/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day97/98Blind35/75;doLeetcodeBlind75-3q/day-5/45Q:completeOn2/15/2023-Day3/15:1.P1:1.numberOfWeakCharactersInTheGameTimed25Mins-x1pomo(6:00-6:30),2.implement-x1pomo(6:30-7:00),3.coinChangeTimed25Mins-x1pomo(7:00-7:30),4.implement-x1pomo(7:30-8:00),5.productOfArrayExceptSelfTimed25Mins-x1pomo(8:00-8:30),6.minStackTimed25Mins-x1pomo(8:30-9:00),7.validateBinarySearchTreeTimed25Mins-x1pomo(9:00-9:30),8.numberOfIslandsTimed25Mins-x1pomo(9:30-10:00),9.rottingOrangesTimed25Mins-x1pomo(10:00-10:30),10.absorber-x1pomo(10:30-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
