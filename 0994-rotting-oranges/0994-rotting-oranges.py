92d82f3e0894cce4d822ff4d2781d62b5ba9d7dd
Rasi <rasi5050@gmail.com>
2023-01-31 07:42:41 -0500

    Time: 59 ms (55.92%), Space: 13.9 MB (42.68%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,6 +1,7 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         #do multi source BFS from all rotten oranges
+        """working code: commented for optimization
         frontier=[]
         numberOfFreshApples=0
         for i in range(len(grid)):
@@ -31,7 +32,30 @@ class Solution:
             firstFlag=False
         # print(numberOfDays, numberOfFreshApples)
         return numberOfDays-2 if numberOfFreshApples==0 else -1
-    
+        """
     #status: correct
     #Analysis: Time O(n), Space O(1); not inccuring additional storage
     #ref: 1/31/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day98/98Blind39/75;doLeetcodeBlind75-3q/day-9/45Q:completeOn2/15/2023-Day4/15:1.numberOfIslandsTimed25Mins-x1pomo(6:00-6:30),2.rottingOrangesTimed25Mins-x1pomo(6:30-7:00),3.searchInARotatedSortedArrayTimed25Mins-x1pomo(7:00-7:30),4.combinationSumTimed25Min-x1pomo(7:30-8:00),5.implement-x1pomo(8:00-8:30),6.couresWork:DAA:flippedClassroom:P1:DAA:fliipedClassroom:watchReccurenceRelation(https://drive.google.com/drive/u/1/folders/0AJMcog-ghtHLUk9PVA)-x4pomo(8:30-10:30),7.absorber-x1pomo(10:30-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
+        #do multi source BFS from all rotten orange nodes, decrement number of fresh oranges each time, at the end check whether number of fresh oranges has become zero or not
+        frontier=[]
+        numberOfFreshOranges=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==2:
+                    frontier.append((i-1,j)); frontier.append((i+1,j)); frontier.append((i,j-1)); frontier.append((i,j+1))
+                elif grid[i][j]==1:
+                    numberOfFreshOranges+=1
+        if numberOfFreshOranges==0: return 0    #if there are no rotten oranges, it takes zero days to make all rotten
+        
+        numberOfDays=0
+        while frontier:
+            next=[]
+            for i,j in frontier:
+                if not (i<0 or j<0 or i>len(grid)-1 or j>len(grid[0])-1) and grid[i][j]==1:
+                    grid[i][j]=2
+                    numberOfFreshOranges-=1
+                    next.append((i-1,j)); next.append((i+1,j)); next.append((i,j-1)); next.append((i,j+1))
+            frontier=next
+            numberOfDays+=1
+                
+        return numberOfDays-1 if numberOfFreshOranges==0 else -1
