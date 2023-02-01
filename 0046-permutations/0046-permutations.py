2c6ddda61f1577dbb9910a0ef97d50a142a37507
Rasi <rasi5050@gmail.com>
2023-02-01 08:59:54 -0500

    Time: 46 ms (53.77%), Space: 14.1 MB (54.07%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        res=[]
+        perm=[]
+        visited=set()
+        def dfs(i):
+            if i==len(nums):
+                res.append(perm.copy())
+                return 
+            if i>len(nums):
+                return
+            for num in nums:
+                if num not in visited:
+                    visited.add(num)
+                    perm.append(num)
+                    dfs(i+1)
+                    perm.pop()
+                    visited.remove(num)
+        dfs(0)
+        return res
