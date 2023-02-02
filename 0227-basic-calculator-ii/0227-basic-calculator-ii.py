f694b5d14c862123b53c930ea87ff71cffd0ca2f
Rasi <rasi5050@gmail.com>
2023-02-02 09:25:33 -0500

    Time: 210 ms (54.14%), Space: 23 MB (5.03%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -76,3 +76,6 @@ class Solution:
             elif curOp=='-':
                 res-=curNum
         return res
+    #status: correct; space trade off, do multiplication and division in first pass, then do another pass for addition and subtraction
+    #Analysis: Time O(n), Space O(n)
+    #ref: 2/2/2023P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day100/100;doLeetcodeBlind75-3q/day-14/45Q:completeOn2/15/2023-Day6/15:prepareTiktokDevOpsFullForceDay1/7;1.BasicCalculator3TImed25Mins-x1pomo(6:00-6:30),2.implement-x1pomo(6:30-7:00),3.courseSchedule2Timed25Mins-x1pomo(7:00-7:30),4.implement-x1pomo(7:30-8:00),5.mergeIntervalsTimed25Mins-x1pomo(8:00-8:30),6.homeWork:DAA-checkAnswers:x1pomo(8:30-9:00),7.homeWork:DAA-createFinalVersion-x1pomo(9:00-9:30),8.homeWork:CAA:q1,a,b,c-x1pomo(9:30-10:00),9.q2,1,2,3-x2pomo(10:00-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
