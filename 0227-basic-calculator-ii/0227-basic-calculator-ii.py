2af47dae82a744eb48ca7b79aa78b440a168d2d3
Rasi <rasi5050@gmail.com>
2023-02-02 09:23:17 -0500

    Time: 229 ms (48.89%), Space: 23.2 MB (5.03%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,40 +1,78 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        #if the current operations has higher precedence than previous operation, reverse previous operations, do the current operation of the variables then add the results to the previous values
+        s=s.replace(' ','')
         
-        operator='+'
-        cur=prev=res=i=0
-        curNumS=""
-        while i < len(s):
-            cur_char=s[i]
-            if cur_char.isdigit():
-                while i < len(s) and s[i].isdigit():
-                    curNumS+=s[i]
-                    i+=1
-                i-=1
-                curNum=int(curNumS)
-                if operator=='+':
-                    res+=curNum
-                    prev=curNum
-                elif operator=='-':
-                    res-=curNum
-                    prev=-curNum
-                elif operator=='*':
-                    res-=prev
-                    res+=prev*curNum
-                    
-                    prev=prev*curNum
-                elif operator=='/':
-                    res-=prev
-                    res+=int(prev/curNum)
-                    
-                    prev = int(prev/curNum)
-                curNumS=""
-            elif cur_char != ' ':
-                operator=s[i]
-            i+=1
+        temp=[]
+        curNum=0
+        for c in s:         #spliting the numbers and operators into temp array
+            if c in {'+','-','*','/'}: 
+                temp.append(curNum)
+                temp.append(c)
+                curNum=0
+            else:
+                curNum=(curNum*10) + int(c)
+        temp.append(curNum)
+        
+        prevOp=None
+        prevNum=temp[0]
+        res=temp[0]
+        print(temp)
+        """try two pass solution
+        for i in range(1,len(temp),2):
+            curOp=temp[i]
+            curNum=temp[i+1]
+    
+            if curOp in {'*','/'} and prevOp in {'+','-'}: #precedence clash
+                #reverse previous operation
+    
+                if prevOp=='+':
+                    if curOp=='*':
+                        res=(res-prevNum)+(prevNum*curNum)
+                    elif curOp=='/':
+                        # print('hit', res)
+                        res=(res-prevNum)+int(prevNum/curNum)    
+                elif prevOp=='-':
+                    if curOp=='*':
+                        res=(res+prevNum)-(prevNum*curNum)
+                    elif curOp=='/':
+                        res=(res+prevNum)-int(prevNum/curNum)
+                prevNum=curNum
+                prevOp=curOp
+            else:
+                if curOp=='+':
+                    res=res+curNum
+                elif curOp=='-':
+                    res=res-curNum
+                elif curOp=='/':
+                    res=int(res/curNum)
+                elif curOp=='*':
+                    res=res*curNum
+                prevNum=curNum
+                prevOp=curOp
         return res
-        
-        #status: correct; complete help from (https://www.youtube.com/watch?v=W3Rg4HVSZ9k&t=939s)
-        #analysis: Time O(n)
-        #Space: O(1), fixed number of variables
-        #ref:11/22/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day45/46:1.(P1:doPaypalLeetcodeQuestion4/5(https://github.com/hxu296/leetcode-company-wise-problems-2022/blob/main/companies/Paypal.csv),2.happyNumberTimed25Mins-x1pomo(5:30-6:00),3.implement-x2pomo(6:00-7:00),4.squaresOfASortedArrayTimed25Mins-x1pomo(7:00-7:30),5.implement-x2pomo(7:30-8:30),6.reverse-substrings-between-each-pair-of-parenthesesTimed25Mins-x1pomo(8:30-9:00),6.implement-x2pomo(9:00-10:00),7.basicCalculatorAnswers-x1pomo(10:00-10:30),8.basicCalculator2Timed25Mins-x1pomo(10:30-11:00),9.implement-x2pomo(11:00-12:00)=x13pomo(5:30-12:00)
+        """
+        afterOnePass=[temp[0]]
+        for i in range(1,len(temp),2):
+            curOp=temp[i]
+            curNum=temp[i+1]
+            if curOp in {'*','/'}:
+                #process mult and division
+                if curOp=='*':
+                    afterOnePass.append(afterOnePass.pop()*curNum)
+                elif curOp=='/':
+                    afterOnePass.append(int(afterOnePass.pop()/curNum))
+            else:
+                afterOnePass.append(curOp)
+                afterOnePass.append(curNum)
+        print(afterOnePass)
+        #do second pass
+        res=afterOnePass[0]
+        for j in range(1, len(afterOnePass),2):
+            curOp=afterOnePass[j]
+            curNum=afterOnePass[j+1]
+            if curOp=='+':
+                res+=curNum
+            elif curOp=='-':
+                res-=curNum
+        return res
