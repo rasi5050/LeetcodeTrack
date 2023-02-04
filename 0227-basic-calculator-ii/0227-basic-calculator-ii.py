62131171d03da21391ff0b646c5d96e221f1116b
Rasi <rasi5050@gmail.com>
2023-02-04 06:55:45 -0500

    Time: 155 ms (79.91%), Space: 16.1 MB (98.13%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -26,4 +26,6 @@ class Solution:
                 currNum=0
                 prevOp=c
         return res
-                
+        #status: correct; -x2nd dayTry
+        #Analysis: Time O(n), Space O(1)
+        #ref: 2/4/2023P2:track1-cpGrind75;Day101/102;doLeetcodeBlind75-3q/day-14/45Q:completeOn2/15/2023-Day7/15:prepareTiktokDevOpsIntenseFocusDay2/7;1.BasicCalculator3TImed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.longestIncreasingPathInAMatrixTimed25Mins-x1pomo(7:30-8:00),4.implement-x1pomo(8:00-8:30),5.mergeIntervalsTimed25Mins-x1pomo(8:30-9:00),6.decodeStringTimed25Mins-x1pomo(9:00-9:30),7.implement-x2pomo(9:30-10:30),8.absorber-x1pomo(10:30-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
