42b1fee4ae6952b21d55919f6358a2bd1c47be57
Rasi <rasi5050@gmail.com>
2023-02-04 11:51:56 -0500

    Time: 61 ms (97.52%), Space: 26.3 MB (59.22%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,17 +7,17 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root in {None, p, q}: return root
-        
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if left and right: return root
-        elif left: return left
-        else: return right
-        
-        #status: success: help from https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/discuss/1306476/C%2B%2BPython-2-solutions-Clean-and-Concise-O(N)
-        #analysis: Time O(n); visiting all nodes, Space O(1); nothing stored
-        #ref: 12/19/2022P2:track1-cpGrind75;3QuestionPerDay(perQuestion-x3pomo),perDay-x10pomo;35hoursperWeek;15hrPerTopic+15hrPerTopic+5hrBuffer;Day55/56,1.lowestCommonAncestorOfABinaryTreeTimed25Mins-x1pomo(10:00-10:30),2.implement-x2pomo(10:30-11:30)-x3pomo(10:00-11:30)
+        lca=None
+        def recurse(root):
+            nonlocal lca
+            if not root:
+                return False
 
-        
+            left,right=recurse(root.left), recurse(root.right)
+           
+            curr = root==p or root==q
+            if curr+left+right>=2:
+                lca=root
+            return curr or left or right
+        recurse(root)
+        return lca
