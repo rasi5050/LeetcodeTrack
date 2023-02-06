b1ff3452f8b3a0c3dc347b1f196dc01bce5639a8
Rasi <rasi5050@gmail.com>
2023-02-06 06:27:28 -0500

    Time: 114 ms (85.06%), Space: 15 MB (33.95%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -21,3 +21,7 @@ class CustomStack:
 # obj.push(x)
 # param_2 = obj.pop()
 # obj.increment(k,val)
+
+#status: correct
+#Analysis: Time O(n), Space O(n)
+#ref: 2/6/2023P2:track1-cpGrind75;Day102/102;doLeetcodeBlind75-3q/day-16/45Q:completeOn2/15/2023-Day10/15:prepareTiktokDevOpsIntenseFocusDay3/7;1.(https://leetcode.com/problems/design-a-stack-with-increment-operation/)-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.(https://leetcode.com/discuss/interview-question/1263982/thomson-reuters-oa-circular-printer)-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.(https://leetcode.com/problems/count-sorted-vowel-strings/discuss/1021180/python-start-and-bars-explained)-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.absorber-x2pomo(10:00-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
