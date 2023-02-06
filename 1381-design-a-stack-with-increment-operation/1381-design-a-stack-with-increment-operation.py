d8cc848ab446b1006720199e450ccc9b94630536
Rasi <rasi5050@gmail.com>
2023-02-06 07:18:36 -0500

    Time: 73 ms (99.51%), Space: 14.8 MB (71.60%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -46,4 +46,8 @@ class CustomStack:
     def increment(self, k: int, val: int) -> None:
         if min(k, len(self.stack))-1>=0: self.inc[min(k, len(self.stack))-1]+=val
         #else: do nothing
-        
+    
+    #status: correct
+    #Analysis: Time O(1), Space O(n)
+    #ref: 2/6/2023P2:track1-cpGrind75;Day102/102;doLeetcodeBlind75-3q/day-16/45Q:completeOn2/15/2023-Day10/15:prepareTiktokDevOpsIntenseFocusDay3/7;1.(https://leetcode.com/problems/design-a-stack-with-increment-operation/)-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.(https://leetcode.com/discuss/interview-question/1263982/thomson-reuters-oa-circular-printer)-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.(https://leetcode.com/problems/count-sorted-vowel-strings/discuss/1021180/python-start-and-bars-explained)-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.absorber-x2pomo(10:00-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
+
