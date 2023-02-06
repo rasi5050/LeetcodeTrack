e01356edd29c5bb9bbd008d8b0ec14cc9d8932cd
Rasi <rasi5050@gmail.com>
2023-02-06 09:32:29 -0500

    Time: 34 ms (68.59%), Space: 13.8 MB (95.53%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -26,3 +26,8 @@ class Solution:
     #help(https://leetcode.com/problems/count-sorted-vowel-strings/discuss/1021180/python-start-and-bars-explained)
     
         return ((n+4)*(n+3)*(n+2)*(n+1))//(4*3*2*1)
+    #status: correct; didnot understand; help(https://leetcode.com/problems/count-sorted-vowel-strings/discuss/1021180/python-start-and-bars-explained)
+    #Analysis: Time O(1), Space O(1)
+    #ref: 2/6/2023P2:track1-cpGrind75;Day102/102;doLeetcodeBlind75-3q/day-16/45Q:completeOn2/15/2023-Day10/15:prepareTiktokDevOpsIntenseFocusDay3/7;1.(https://leetcode.com/problems/design-a-stack-with-increment-operation/)-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30),3.(https://leetcode.com/discuss/interview-question/1263982/thomson-reuters-oa-circular-printer)-x1pomo(7:30-8:00),4.implement-x2pomo(8:00-9:00),5.(https://leetcode.com/problems/count-sorted-vowel-strings/discuss/1021180/python-start-and-bars-explained)-x1pomo(9:00-9:30),6.implement-x2pomo(9:30-10:30),7.absorber-x2pomo(10:00-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
+
+    
