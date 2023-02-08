eabe54ecb270306cc95ef0b1390a88ad1f1dd159
Rasi <rasi5050@gmail.com>
2023-02-08 09:22:52 -0500

    Time: 214 ms (64.19%), Space: 24.1 MB (22.98%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.py
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -21,5 +21,14 @@ class Solution:
             if account[1] not in visited:
                 ans.append([account[0]] + sorted(dfs(account[1])))
         return ans
-                
+        """
+        #synopsis: emails are connected as a to b, so that connected components will be identified as to a single person and a dfs() on any of his emails will lead to all other emails that he owns. 
+        graph is represented as {a:b
+                                b:c
+                                c:a}
         
+        for each first email of a person , we do a dfs, esesntially retrieving his entire connected emails, visited list prevents from duplicating
+        """
+        #status: correct; help(https://leetcode.com/problems/accounts-merge/discuss/1601960/C%2B%2BPython-Simple-Solution-w-Images-and-Explanation-or-Building-Graph-and-DFS)
+        #Analysis: Time O(NML*log(MN)), help(https://leetcode.com/problems/accounts-merge/discuss/1601960/C%2B%2BPython-Simple-Solution-w-Images-and-Explanation-or-Building-Graph-and-DFS)
+        #ref: 2/8/2023P2:track1-cpGrind75;Day104/104;doLeetcodeBlind75-3q/day-17/45Q:completeOn2/15/2023-Day12/15,collateQuestionPatternIntoCategoriesAndTemplaye-Day3/3:(addWordDescriptionOnceProblemIsSolvedDay2/2)1.(accountsMergeTimed25Mins)-x1pomo(6:00-6:30),2.implement(addDescriptionAfterSolvingProblem)-x1pomo(6:30-7:00),3.sortColorsTimed25Mins-x1pomo(7:00-7:30),4.implement(addDescriptionAfterSolvingProblem)-x1pomo(7:30-8:00),5.absorber-x2pomo(8:00-9:00),6.wordBreakTimed25Mins-x1pomo(9:00-9:30),7.implement-x1pomo(9:30-10:00),8.applyInternships(1.https://www.tesla.com/careers/search/job/software-data-platforms-engineering-internship-summer-2023-153313?source=LinkedIn,2.https://www.applicantpro.com/openings/netcentrics/jobs/2753633-478257,3.https://careers.intuitive.com/us/en/job/JOB2747/DevOps-Intern?utm_source=linkedin&utm_medium=phenom-feeds,4.https://boards.greenhouse.io/schonfeld/jobs/4839329?gh_src=e73f47fb1us,5.https://jobs.lever.co/milhouseinc/923fc470-3ddf-4d35-87b2-2699657fcfcb,6.https://jobs.keysight.com/job/Loveland-R&D-DevOps-Internship-CO-80534/980654100/?feedId=217500&utm_source=LILimitedListings&utm_campaign=Keysight_LinkedinLL)-x2pomo(10:00-11:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
