16e34c26df88443bbe077139c33c12567fc43972
Rasi <rasi5050@gmail.com>
2023-02-09 07:28:59 -0500

    Time: 39 ms (49.77%), Space: 13.7 MB (96.23%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """working code in 2 pass; commented for alter
         #use array of size 3:
         bucket=[0]*3
         for n in nums:
@@ -10,5 +11,16 @@ class Solution:
         
         for i,v in enumerate([0]*bucket[0]+[1]*bucket[1]+[2]*bucket[2]):
             nums[i]=v
-            
+        """
+        l,i,r=0,0,len(nums)-1
+        for _ in range(len(nums)):
+            if nums[i]==0:
+                nums[l],nums[i]=nums[i],nums[l]
+                i+=1
+                l+=1
+            elif nums[i]==2:
+                nums[r],nums[i]=nums[i],nums[r]
+                r-=1
+            elif nums[i]==1:
+                i+=1
                 
