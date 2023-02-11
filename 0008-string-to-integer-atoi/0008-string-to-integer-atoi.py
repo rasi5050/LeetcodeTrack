7af7f0e5d6a2ec021eac5141145c28a82664a03f
Rasi <rasi5050@gmail.com>
2023-02-11 08:50:50 -0500

    Time: 22 ms (99.73%), Space: 13.9 MB (70.83%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -15,6 +15,10 @@ class Solution:
         res=res*sign
         res=min(res, 2**31-1) if res>=0 else max(res,-2**31)
         return res
+    #status: correct; help(https://leetcode.com/problems/string-to-integer-atoi/discuss/3110465/Python3-Fast-Code-and-Simple)
+    #Analysis: Time O(n), Space O(1)
+    #ref: 2/11/2023P2:track1-cpGrind75;Day107/108;doLeetcodeBlind75-3q/day-21/45Q:completeOn2/15/2023-Day15/15,collateQuestionPatternIntoCategoriesAndTemplate-Day6/5:(addWordDescriptionOnceProblemIsSolvedDay5/4),               1.stringToIntegerTimed25Mins-x1pomo(6:00-6:30),2.implement-x2pomo(6:30-7:30)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)shortened(6:00-7:30)
+
                 
             
                 
