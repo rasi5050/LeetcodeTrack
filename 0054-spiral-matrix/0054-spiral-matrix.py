3f73f5301a25435c173b329e0af7f5162681e520
Rasi <rasi5050@gmail.com>
2023-02-12 08:41:21 -0500

    Time: 38 ms (33.52%), Space: 13.9 MB (26.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,28 +4,28 @@ class Solution:
         left,right,top,bottom=-1,len(matrix[0]),-1,len(matrix)
         res=[]
         (i,j)=(0,0)
-        while top<i<bottom or left<j<right:
-            while j<right and matrix[i][j]!='#':   #traverse right
+        while top<i<bottom and left<j<right:
+            while j<right and (top!=bottom-1 and left!=right-1):   #traverse right
+                print('hit1', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
-                matrix[i][j]='#'
                 (i,j)=(i,j+1)
             (i,j)=(i+1,j-1)
             top+=1
-            while i<bottom and matrix[i][j]!='#':   #traverse down
+            while i<bottom and (top!=bottom-1 and left!=right-1):   #traverse down
+                print('hit2', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
-                matrix[i][j]='#'
                 (i,j)=(i+1,j)
             (i,j)=(i-1,j-1)    
             right-=1
-            while j>left and matrix[i][j]!='#':   #traverse left
+            while j>left and (top!=bottom-1 and left!=right-1):   #traverse left
+                print('hit3', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
-                matrix[i][j]='#'
                 (i,j)=(i, j-1)
             (i,j)=(i-1, j+1)
             bottom-=1
-            while i>top and matrix[i][j]!='#':   #traverse up
+            while i>top and (top!=bottom-1 and left!=right-1):   #traverse up
+                print('hit4', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
-                matrix[i][j]='#'
                 (i,j)=(i-1,j)
             (i,j)=(i+1,j+1)
             left+=1
