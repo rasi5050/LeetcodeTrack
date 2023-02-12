4cd052e336bdf1975fc1367ef7e3f6b696a24cd0
Rasi <rasi5050@gmail.com>
2023-02-12 08:45:35 -0500

    Time: 30 ms (80.89%), Space: 14 MB (26.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -26,3 +26,9 @@ class Solution:
             (i,j)=(i+1,j+1)
             left+=1
         return res 
+    
+    #synopsis: fix 4 boundaries top, bottom, left, right. then iterete to right->down->left->up using (i,j)
+    # when a direction is complete set pointer a place back then to the next possible candidate. edge case is a single row or column, it can be identified using marking visited elements as '#' or checking the (top!=bottom-1)
+    #status: correct; took 40mins to complete
+    #Analsysis: Time O(m*n), Space O(m*n)
+    #ref: 2/12/2023P2:track1-cpGrind75;Day107/108;doLeetcodeBlind75-3q/day-21/45Q:completeOn2/15/2023-Day15/15,collateQuestionPatternIntoCategoriesAndTemplate-Day6/5:(addWordDescriptionOnceProblemIsSolvedDay5/4),1.sprialMatrixTimed25Mins-x1pomo(6:30-7:00),2.implement-x2pomo(7:00-8:00)-x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)shortened(6:30-8:00)
