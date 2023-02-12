af02022425a669732b40c8d75e1e3e228b74ace5
Rasi <rasi5050@gmail.com>
2023-02-12 08:41:42 -0500

    Time: 35 ms (52.49%), Space: 14 MB (26.17%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -6,25 +6,21 @@ class Solution:
         (i,j)=(0,0)
         while top<i<bottom and left<j<right:
             while j<right and (top!=bottom-1 and left!=right-1):   #traverse right
-                print('hit1', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
                 (i,j)=(i,j+1)
             (i,j)=(i+1,j-1)
             top+=1
             while i<bottom and (top!=bottom-1 and left!=right-1):   #traverse down
-                print('hit2', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
                 (i,j)=(i+1,j)
             (i,j)=(i-1,j-1)    
             right-=1
             while j>left and (top!=bottom-1 and left!=right-1):   #traverse left
-                print('hit3', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
                 (i,j)=(i, j-1)
             (i,j)=(i-1, j+1)
             bottom-=1
             while i>top and (top!=bottom-1 and left!=right-1):   #traverse up
-                print('hit4', matrix[i][j], (i,j))
                 res.append(matrix[i][j])
                 (i,j)=(i-1,j)
             (i,j)=(i+1,j+1)
