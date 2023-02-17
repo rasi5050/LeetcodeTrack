01aefe68d79b15d24818f79f0d7cc7d2f94d512f
Rasi <rasi5050@gmail.com>
2023-02-17 06:22:54 -0500

    Time: 28 ms (89.07%), Space: 14.4 MB (5.72%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,7 +1,7 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         #normal approach
-        
+        """
         @cache
         def dfs(i,j):
             if i>=m or j>=n:
@@ -13,6 +13,19 @@ class Solution:
             count+=dfs(i,j+1)
             return count
         return dfs(0,0)
+        """
+        #alternate from bottom to top
+        @cache
+        def dfs(i,j):
+            if i<0 or j<0:
+                return 0
+            if i==0 and j==0:
+                return 1
+            count=0
+            count+=dfs(i-1,j)
+            count+=dfs(i,j-1)
+            return count
+        return dfs(m-1,n-1)
         
         
         
