832d3383da563eeee799b87fdc6dedfc8ac5c445
Rasi <rasi5050@gmail.com>
2023-02-17 09:51:56 -0500

    Time: 186 ms (57.35%), Space: 88.5 MB (51.36%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,11 +6,20 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
-            return None
-        root = TreeNode(preorder[0])
-        mid=inorder.index(preorder[0])
-        root.left=self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right=self.buildTree(preorder[mid+1:], inorder[mid+1:])
-        return root
-       
+        
+        def recurse(preorder, inorder):
+            if not preorder:
+                return None
+            node=TreeNode(preorder[0])
+            
+            i=inorder.index(preorder[0])            
+            node.left=recurse(preorder[1:i+1], inorder[:i])
+            node.right=recurse(preorder[i+1:], inorder[i+1:])
+            return node
+        return recurse(preorder, inorder)
+            
+        #storing global inorder indexes doesnt work because, when the preorder is divided and recursed
+        #the inorder should also reflect the preorder elements, then only the index will match
+        
+        
+        
