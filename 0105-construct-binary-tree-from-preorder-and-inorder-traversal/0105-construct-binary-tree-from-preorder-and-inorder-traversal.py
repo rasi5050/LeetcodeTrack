bc465b923a1028dc4ee3e5fa5e4be136bb5335b8
Rasi <rasi5050@gmail.com>
2023-02-17 09:58:25 -0500

    Time: 194 ms (52.04%), Space: 88.7 MB (22.96%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,7 +8,7 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         
         def recurse(preorder, inorder):
-            if not preorder:
+            if not preorder:        #inorder not be checked because when preorder is empty inorder will be empty        
                 return None
             node=TreeNode(preorder[0])
             
@@ -20,6 +20,11 @@ class Solution:
             
         #storing global inorder indexes doesnt work because, when the preorder is divided and recursed
         #the inorder should also reflect the preorder elements, then only the index will match
+        #the inorder list is used as a index finder to the preorder
         
+        #status: correct; didnt understand; help(https://www.youtube.com/watch?v=ihj4IQGZ2zc&t=1s)
+        #Analysis: Time O(no of nodes), Space O(no. of nodes)
+        #ref: 2/17/2023P2:track1-cpGrind75;Day111/112;doLeetcodeBlind75-3q/day-26/45Q:completeOn2/15/2023-Day19/15,collateQuestionPatternIntoCategoriesAndTemplate-Day9/5:(addWordDescriptionOnceProblemIsSolvedDay6/4),1.uniquePathsTimed25Mins-x1pomo(6:00-6:30),2.implement-x1pomo(6:30-7:00),3.constructABinaryTreeFromPreorderAndInorderTraversalTimed25Mins-x1pomo(7:00-7:30),4.implement-x2pomo(7:30-8:30),5.containerWithMostWaterTimed25Mins-x1pomo(8:30-9:00),6.implement-x1pomo(9:00-9:30),7.applyInternship-x6SdeFromGitHubPage-x2pomo(9:30-10:30),8.applyInternship-x3DevOpsFromLinkedIn-x1pomo(10:30-11:00)             -x12pomo(5:30-11:30)alter-x10pomo(6:00-11:00)
+
         
         
