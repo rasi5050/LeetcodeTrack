4586c36700c8ab3b410b8e1312cd681a787e8fb2
Rasi <rasi5050@gmail.com>
2023-02-20 09:00:26 -0500

    Time: 7440 ms (9.48%), Space: 15.3 MB (26.18%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,25 +1,8 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        #use primes
-        primes={'a': 2,'b': 3,'c': 5,'d': 7,'e': 11,'f': 13,'g': 17,'h': 19,'i': 23,'j': 29,'k': 31,'l': 37,'m': 41,'n': 43,'o': 47,'p': 53,'q': 59,'r': 61,'s': 67,'t': 71,'u': 73,'v': 79,'w': 83,'x': 89,'y': 97,'z': 101}
-        
-        pLen=len(p)
-
-        target=prod([primes[c] for c in p])
-        
-        anag = prod(primes[c] for c in s[:pLen])
-        if len(s)==len(p):
-            if anag==target: return [0]
-            else: return []
+        c=Counter(p)
         res=[]
-        i=0
-        for i in range(len(s)-pLen):
-            if anag==target:
+        for i in range(len(s)-len(p)+1):
+            if Counter(s[i:i+len(p)])==c:
                 res.append(i)
-            print(i)
-            anag=(anag//primes[s[i]])*primes[s[i+pLen]]
-        if anag==target:
-            res.append(i+1)
         return res
-                
-            
