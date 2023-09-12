c4885f0ccede5a8de7117d14187e59ddecf4e987
Rasi <rasi5050@gmail.com>
2023-09-12 18:44:15 -0400

    Time: 22 ms (41.52%), Space: 13.4 MB (25.15%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -7,21 +7,38 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        mTemp=nTemp=0
-        tempArray=[]
-        while (mTemp<m and nTemp<n):
-            if nums1[mTemp]<=nums2[nTemp]:
-                tempArray.append(nums1[mTemp])
-                mTemp+=1
+        # mTemp=nTemp=0
+        # tempArray=[]
+        # while (mTemp<m and nTemp<n):
+        #     if nums1[mTemp]<=nums2[nTemp]:
+        #         tempArray.append(nums1[mTemp])
+        #         mTemp+=1
+        #     else:
+        #         tempArray.append(nums2[nTemp])
+        #         nTemp+=1
+        # if (mTemp<m):
+        #     tempArray.extend(nums1[mTemp:])
+        # else:
+        #     tempArray.extend(nums2[nTemp:])
+        # for i in range(m+n):
+        #     nums1[i]=tempArray[i]
+        
+        #optimal method
+        i,j,k=m-1,n-1,m+n-1
+        
+        if j==-1:
+            return
+
+            
+        while (j>-1 and k>-1):
+            if i>-1 and nums1[i] > nums2[j]:
+                nums1[k]=nums1[i]
+                i-=1
             else:
-                tempArray.append(nums2[nTemp])
-                nTemp+=1
-        if (mTemp<m):
-            tempArray.extend(nums1[mTemp:])
-        else:
-            tempArray.extend(nums2[nTemp:])
-        for i in range(m+n):
-            nums1[i]=tempArray[i]
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
+            
         
                 
         
